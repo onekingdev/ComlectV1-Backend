fe8e71b6b59748fef3c29701959116facb005e0f
Ivan Vega <ivanvega@gmail.com>
2016-06-04 00:22:34 -0500

    Precompile fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor/assets/componen
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << %r{.*.(?:eot|svg|ttf|woff2)$}
