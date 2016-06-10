7e85d09769c23e13456dc0399f2c0619e5161085
Ivan Vega <ivanvega@gmail.com>
2016-06-09 19:34:13 -0500

    "Selectize" business employees

--- app/models/business.rb
+++ app/models/business.rb
@@ -6,14 +6,17 @@ class Business < ActiveRecord::Base
 
   include ImageUploader[:logo]
 
+  INDUSTRIES = %w(Broker-Dealer Banking Fund\ Manager Investment\ Adviser Commodities\ Trader).freeze
+  EMPLOYEE_OPTIONS = %w(<10 11-50 51-100 100+).freeze
+
   validates :contact_first_name, :contact_last_name, :contact_email, presence: true
   validates :business_name, :industry, :employees, :description, presence: true
   validates :country, :city, :state, presence: true
   validates :description, length: { maximum: 750 }
+  validates :industry, inclusion: { in: INDUSTRIES }
+  validates :employees, inclusion: { in: EMPLOYEE_OPTIONS }
 
   accepts_nested_attributes_for :user
-
-  INDUSTRIES = %w(Broker-Dealer Banking Fund\ Manager Investment\ Adviser Commodities\ Trader).freeze
 
   def self.for_signup(attributes = {})
     new(attributes).tap do |business|
