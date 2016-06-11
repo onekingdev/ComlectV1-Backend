c40da3845362c44f0783d4a5d496f60b936f7a89
Ivan Vega <ivanvega@gmail.com>
2016-06-10 19:22:43 -0500

    Fix flash class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module ApplicationHelper
   def render_flash
-    classes = { alert: 'warning', notice: 'notice' }
+    classes = { alert: 'warning', notice: 'info' }
     flash.keys.map do |key|
       content_tag 'div', flash[key], class: "alert alert-#{classes[key.to_sym]}"
     end.join("\n").html_safe
