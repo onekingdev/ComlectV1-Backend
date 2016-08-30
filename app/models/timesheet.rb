3d61d888a94cf1cf80aa479c6cbdf7009e429815
Ivan Vega <ivanvega@gmail.com>
2016-08-30 14:34:28 -0500

    Limit timesheet submission on complete projects

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -13,6 +13,7 @@ class Timesheet < ActiveRecord::Base
   accepts_nested_attributes_for :time_logs, allow_destroy: true
 
   validates :time_logs, presence: true
+  validate :validate_project_is_active
 
   def total_due
     if errors.any?
@@ -21,5 +22,12 @@ class Timesheet < ActiveRecord::Base
     else
       time_logs.sum(:hours) * project.hourly_rate
     end
+  end
+
+  private
+
+  def validate_project_is_active
+    changes = self.changes.any? || time_logs.any? { |log| log.changes.any? }
+    errors.add :base, 'Cannot add/update on complete projects' if changes && project.complete?
   end
 end
