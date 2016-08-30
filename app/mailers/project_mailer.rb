d2036416131feeccf30f442add768d4f30deed69
Ivan Vega <ivanvega@gmail.com>
2016-08-30 11:55:12 -0500

    Tweak "Share project" messages

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -11,13 +11,29 @@ class ProjectMailer < ApplicationMailer
          }
   end
 
-  def share(project, name, email, message)
-    @message = message
+  def share(project, name, email, message_text, message_html)
     @project_url = project_url(project)
+    @message_text = add_project_link(project, message_text, @project_url, :text)
+    @message_html = add_project_link(project, message_html, @project_url, :html)
     mail to: "#{name} <#{email}>", subject: "#{project.title} on Complect"
   end
 
   private
+
+  def add_project_link(project, text, url, format = :html)
+    found = text =~ /%\{project_job_link\}/
+    replacement = case [format, !found.nil?]
+                  when [:html, true]
+                    "<a href='#{url}'>#{project.title}</a>".html_safe
+                  when [:html, false]
+                    "<p>Project Link: <a href='#{url}'>#{project.title}</a></p>".html_safe
+                  when [:text, true]
+                    url
+                  else # [:text, false]
+                    "\n\nProject Link: #{url}"
+                  end
+    found ? text.gsub('%{project_job_link}', replacement) : text + replacement
+  end
 
   def invite_message_html(invite)
     url = project_url(invite.project)
--- app/models/project/share.rb
+++ app/models/project/share.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 class Project::Share
   include ActiveModel::Model
+  include ActionView::Helpers::TextHelper
 
-  attr_accessor :name, :email, :message, :project, :user
+  attr_accessor :name, :email, :message, :message_html, :project, :user
 
   validates :name, :email, presence: true
   validates :message, presence: true, if: -> { user.business }
@@ -10,26 +11,40 @@ class Project::Share
   def self.for(project, attributes = {})
     new(attributes).tap do |share|
       share.project = project
-      share.set_message
+      share.user.specialist ? share.set_specialist_message : share.set_business_message
     end
   end
 
-  def set_message
-    return self.message = specialist_message if user.specialist
+  def set_specialist_message
+    self.message = specialist_message_text
+    self.message_html = specialist_message_html
+  end
+
+  def set_business_message
     self.message = business_message unless message.present?
+    self.message_html = simple_format message
   end
 
   def send!
-    ProjectMailer.deliver_later :share, project, name, email, message
+    ProjectMailer.deliver_later :share, project, name, email, message, message_html
   end
 
   private
 
-  def specialist_message
+  def specialist_message_text
     I18n.t("project_shares.message_template.from_specialist",
            recipient_name: name,
            sender_name: user.specialist.first_name,
            project_job: "#{project.title} #{project.one_off? ? 'project' : 'job'}").strip
+  end
+
+  def specialist_message_html
+    simple_format I18n.t(
+      "project_shares.message_template.from_specialist",
+      recipient_name: name,
+      sender_name: user.specialist.first_name,
+      project_job: "%{project_job_link} #{project.one_off? ? 'project' : 'job'}"
+    ).strip
   end
 
   def business_message
