90252310e7cbca0f70fa5fa7622cfa71094f2977
Pablo Rozanski <pabloroz@gmail.com>
2016-09-02 02:25:39 -0300

    Replace specialist timesheet buttons with icons

--- app/models/timesheet/decorator.rb
+++ app/models/timesheet/decorator.rb
@@ -3,19 +3,6 @@ class Timesheet::Decorator < Draper::Decorator
   decorates Timesheet
   delegate_all
 
-  def specialist_button
-    label, url = case status
-                 when Timesheet.statuses[:approved]
-                   ['View', h.project_timesheet_path(project, self)]
-                 when Timesheet.statuses[:submitted]
-                   %w(Pending #)
-                 else
-                   ['Edit', h.edit_project_timesheet_path(project, self)]
-                 end
-
-    h.link_to label, url, remote: true, class: "btn btn-primary #{'disabled' if submitted?}"
-  end
-
   def total_hours
     (time_logs.map(&:hours).compact.reduce(:+) || 0.0).to_f.round(2)
   end
