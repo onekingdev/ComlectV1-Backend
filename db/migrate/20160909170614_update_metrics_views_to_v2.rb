74175ebb190f6f6e45a37ea027558cf4ddd2bab4
Ivan Vega <ivanvega@gmail.com>
2016-09-09 12:11:10 -0500

    Update db to use new fee type values for projects

---
+++ db/migrate/20160909170614_update_metrics_views_to_v2.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+class UpdateMetricsViewsToV2 < ActiveRecord::Migration
+  def change
+    replace_view :metrics_jobs_installment_pay, version: 2, revert_to_version: 1
+    replace_view :metrics_jobs_upfront_pay, version: 2, revert_to_version: 1
+  end
+end
