c9f4d2b88dc74bdea89e4d33119bd300f68eac24
Pablo Rozanski <pabloroz@gmail.com>
2016-09-15 00:32:58 -0300

    Test changes to devise mailer

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+class DeviseMailer < ApplicationMailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default from: "Complect <#{ENV.fetch('DEFAULT_MAIL_FROM')}>"
+
+  def reset_password_instructions(record, token, _opts = {})
+    @resource = record
+    @token = token
+    mail(
+      to: @resource.email,
+      template_id: ENV.fetch('POSTMARK_TEMPLATE_ID'),
+      template_model: {
+        subject: 'Reset password instructions',
+        message_html: render('reset_password_instructions.html'),
+        message_text: render('reset_password_instructions.text')
+      }
+    )
+  end
+end
