fd6f6343ca2b296eba381ed5d137b2f12c0cf384
Pablo Rozanski <pabloroz@gmail.com>
2016-09-16 10:38:11 -0300

    Add many more project fields to project edit

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -50,14 +50,41 @@ ActiveAdmin.register Project do
                 industry_ids: [],
                 jurisdiction_ids: []
   form do |f|
-    inputs do
-      input :title
-      input :location
-      input :industries
-      input :jurisdictions
-      input :description, as: :text
-      input :key_deliverables
-      input :specialist
+    f.inputs do
+      f.input :title
+      f.input :location
+      f.input :industries
+      f.input :jurisdictions
+      f.input :description, as: :text
+      f.input :key_deliverables
+      f.input :specialist
+      f.input :starts_on
+      f.input :ends_on
+      f.input :payment_schedule, collection: Project::PAYMENT_SCHEDULES
+      f.input :fixed_budget
+      f.input :hourly_rate
+      f.input :estimated_hours
+      f.input :minimum_experience, collection: Project::MINIMUM_EXPERIENCE
+      f.input :only_regulators
+      f.input :annual_salary
+      f.input :fee_type, collection: Project.fee_types
+      f.input :pricing_type, as: :readonly
+      f.input :calculated_budget, as: :readonly
+      f.input :job_applications_count, as: :readonly
+    end
+    f.inputs 'Extensions' do
+      f.has_many :extensions do |a|
+        a.input :status, collection: ProjectExtension.statuses
+      end
+    end
+    f.inputs 'Timesheets' do
+      f.has_many :timesheets do |a|
+        a.input :status, collection: Timesheet.statuses
+        a.has_many :time_logs do |b|
+          b.input :description
+          b.input :hours, min: 0
+        end
+      end
     end
     f.actions
   end
