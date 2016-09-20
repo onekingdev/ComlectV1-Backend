45a315f5273b00bb5cb84104fef1b703dc8acdf5
Pablo Rozanski <pabloroz@gmail.com>
2016-09-20 02:48:50 -0300

    Dont allow admin user delete in admin panel

--- app/policies/admin_user_policy.rb
+++ app/policies/admin_user_policy.rb
@@ -32,15 +32,15 @@ class AdminUserPolicy < ApplicationPolicy
   end
 
   def destroy?
-    user.super_admin? && user != record
+    false
   end
 
   def destroy_all?
-    user.super_admin?
+    false
   end
 
   def toggle_suspend?
-    destroy?
+    user.super_admin? && user != record
   end
 
   def scope
