8fda58eef6b73061f430767b7e9f94d859a1c3ee
Ivan Vega <ivanvega@gmail.com>
2016-10-05 15:26:27 -0500

    Business financials on dashboard

--- app/controllers/business_dashboard_controller.rb
+++ app/controllers/business_dashboard_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 class BusinessDashboardController < ApplicationController
-  before_action :authenticate_user!
   before_action :require_business!
 
   def show
     @business = current_user.business
+    @financials = Business::Financials.for(current_business)
   end
 end
