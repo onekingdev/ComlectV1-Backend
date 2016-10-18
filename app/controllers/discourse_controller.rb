b4db339755b4ec171962c469599ea3e276c1e018
Niel Malhotra <nielmalhotra89@gmail.com>
2016-10-18 02:10:03 -0400

    Create discourse sso integration

---
+++ app/controllers/discourse_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+class DiscourseController < ApplicationController
+  def specialist_sso
+    if current_user.specialist.blank?
+      redirect_to root_url
+    else
+      sso = form_specialist_sso(current_specialist)
+      redirect_to sso.to_url("https://discourse-specialist.herokuapp.com/session/sso_login")
+    end
+  end
+
+  def business_sso
+    if current_user.business.blank?
+      redirect_to root_url
+    else
+      sso = form_business_sso(current_business)
+      redirect_to sso.to_url("https://discourse-business.herokuapp.com/session/sso_login")
+    end
+  end
+
+  private
+
+  def form_specialist_sso(specialist)
+    secret = ENV['DISCOURSE_SECRET']
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = specialist.user.email
+    sso.name = specialist.full_name
+    sso.username = specialist.user.email
+    sso.external_id = specialist.id
+    sso.sso_secret = secret
+    sso
+  end
+
+  def form_business_sso(business)
+    secret = ENV['DISCOURSE_SECRET']
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = business.user.email
+    sso.name = business.contact_full_name
+    sso.username = business.user.email
+    sso.external_id = business.id
+    sso.sso_secret = secret
+    sso
+  end
+end
