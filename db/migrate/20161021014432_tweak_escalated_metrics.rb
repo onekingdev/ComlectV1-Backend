9c1b60649bd6e98dd799e600739c58e58971865b
Ivan Vega <ivanvega@gmail.com>
2016-10-20 20:45:27 -0500

    Escalated project metric should be cumulative

---
+++ db/migrate/20161021014432_tweak_escalated_metrics.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class TweakEscalatedMetrics < ActiveRecord::Migration
+  def change
+    replace_view :metrics_escalated_projects, version: 2, revert_to_version: 1
+  end
+end
