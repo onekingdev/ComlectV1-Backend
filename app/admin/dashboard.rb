55ab6ff25578568b08c6cf81aabfd1f0c3c2c1e8
Ivan Vega <ivanvega@gmail.com>
2016-10-26 19:40:45 -0500

    Simple dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,11 +2,24 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span "Welcome to Complect"
-        small "WIP"
+  content title: 'Dashboard' do
+    columns do
+      column do
+        panel 'Money Managed' do
+          h1 number_to_currency(Transaction.sum(:amount_in_cents) / 100.0)
+        end
+      end
+
+      column do
+        panel 'Businesses' do
+          h1 Business.count
+        end
+      end
+
+      column do
+        panel 'Specialists' do
+          h1 Specialist.count
+        end
       end
     end
   end
