4beaa9b4bf069b08dc097a4f90b823be827a8adc
Ivan Vega <ivanvega@gmail.com>
2017-01-04 18:23:20 -0600

    Notify business on failed Stripe charges

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 class StripeMailer < ApplicationMailer
-  def verification_needed
+  def verification_needed(email)
     mail to: email,
          template_id: ENV.fetch('POSTMARK_TEMPLATE_ID'),
          template_model: {
            subject: 'Account verification needed',
            message_html: render('verification_needed.html'),
            message_text: render('verification_needed.text')
+         }
+  end
+
+  def charge_failed(email)
+    mail to: email,
+         template_id: ENV.fetch('POSTMARK_TEMPLATE_ID'),
+         template_model: {
+           subject: 'Charge failed',
+           message_html: render('charge_failed.html'),
+           message_text: render('charge_failed.text')
          }
   end
 end
--- app/models/stripe_event/charge_failed.rb
+++ app/models/stripe_event/charge_failed.rb
@@ -4,5 +4,6 @@ class StripeEvent::ChargeFailed < StripeEvent
     transaction = Transaction.find_by!(stripe_id: event.data.object.id)
     transaction.error!
     transaction.update_attribute :status_detail, event.data.object.failure_message
+    StripeMailer.deliver_later :charge_failed, transaction.business.user.email
   end
 end
