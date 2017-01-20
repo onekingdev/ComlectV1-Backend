3bcb7999929fa66dbc10f4d66ba52ba17a066b17
Ivan Vega <ivanvega@gmail.com>
2017-01-20 14:42:46 -0600

    (Admin) Allow deleting flagged content

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Answer < ActiveRecord::Base
   belongs_to :question
-  has_many :flags
+  has_many :flags, as: :flagged_content, dependent: :nullify
   validates :question, presence: true
   validates :text, presence: true
   delegate :project, to: :question, allow_nil: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :project
   belongs_to :specialist
   has_one :answer
-  has_many :flags
+  has_many :flags, as: :flagged_content, dependent: :nullify
   validates :project, presence: true
   validates :text, presence: true
   validates :specialist, presence: true
