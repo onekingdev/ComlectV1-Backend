f64c4c155a82170c162ab54b5af3c32896b0910f
Ivan Vega <ivanvega@gmail.com>
2017-01-31 21:37:27 -0600

    Don't end escalated projects

--- app/jobs/end_projects_job.rb
+++ app/jobs/end_projects_job.rb
@@ -5,7 +5,7 @@ class EndProjectsJob < ActiveJob::Base
   def perform(project_id = nil)
     return process_all if project_id.nil?
     project = Project.find_by(id: project_id)
-    return unless project&.ending?
+    return unless project&.ending? && !project.escalated?
     Project::Ending.process! project
   end
 
@@ -13,6 +13,7 @@ class EndProjectsJob < ActiveJob::Base
 
   def process_all
     Project.ending.each do |project|
+      next if project.escalated?
       self.class.perform_later project.id
     end
   end
