af9c53793f17cde5f800898d6fc262d954b606d6
danst <danst@dansts-iMac.local>
2017-05-08 20:54:41 +0300

    Fix for no-rating; nav z-index 1001

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -9,7 +9,7 @@ module RatingsHelper
     #  end.join("\n").html_safe
     #end
     content_tag(:div, nil, class: "stars_bg #{css_class}") do
-      content_tag(:div, nil, class: "stars_fg", style: "width: #{rating*100.0/5}%")
+      content_tag(:div, nil, class: "stars_fg", style: "width: #{rating*100.0/5}%") if rating
     end
   end
 
