317790ba7b89c9a0fac4d6b7a965f79bcb3a0dc3
Mark Tran <mark.tran@gmail.com>
2017-06-09 17:44:20 +0700

    call PaymentCycle inline. fixes race condition.

--- app/models/job_application/accept.rb
+++ app/models/job_application/accept.rb
@@ -21,7 +21,7 @@ class JobApplication::Accept < Draper::Decorator
 
   def schedule_one_off_fees
     return unless project.one_off?
-    ScheduleChargesJob.perform_later project.id
+    PaymentCycle.for(project).create_charges_and_reschedule!
   end
 
   def schedule_full_time_fees
