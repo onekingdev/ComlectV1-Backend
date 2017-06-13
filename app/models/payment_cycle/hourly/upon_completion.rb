5c07d71aa8599bf619484c05f9600eb0ff91bfb1
Mark Tran <mark.tran@gmail.com>
2017-06-13 17:23:07 +0700

    accumulate scheduled charges before project completion for Upon Completion projects

--- app/models/payment_cycle/hourly/upon_completion.rb
+++ app/models/payment_cycle/hourly/upon_completion.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 class PaymentCycle::Hourly::UponCompletion < PaymentCycle::Hourly
-  def create_charges!
-    # Only charge after project is deemed complete
-    super if project.complete?
-  end
-
   private
 
   def occurrences
