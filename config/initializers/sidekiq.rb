118964bb5045d9c74e6066c39917c512a0838ace
Mark Tran <mark.tran@gmail.com>
2017-06-13 17:38:23 +0700

    only initialize Sidekiq::Cron on Sidekiq server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -22,7 +22,7 @@ end
 SidekiqUniqueJobs.config.unique_args_enabled = true
 
 schedule_file = Rails.root.join('config/schedule.yml')
-if File.exist?(schedule_file)
+if File.exist?(schedule_file) && Sidekiq.server?
   schedule = YAML.load_file(schedule_file)
   Sidekiq::Cron::Job.load_from_hash schedule
 end
