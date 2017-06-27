2e106831586b1630699cab2b9ff4e4d395261d77
Mark Tran <mark.tran@gmail.com>
2017-06-27 14:23:32 +0700

    add debugging logging

--- app/models/payment_cycle/hourly/monthly.rb
+++ app/models/payment_cycle/hourly/monthly.rb
@@ -3,6 +3,12 @@ class PaymentCycle::Hourly::Monthly < PaymentCycle::Hourly
   private
 
   def occurrences
+    Rails.logger.info '3' * 80
+    Rails.logger.info project.starts_on
+    Rails.logger.info project.ends_on
+    Rails.logger.info schedule.inspect
+    Rails.logger.info schedule.occurrences_between(project.starts_on, project.ends_on)[1..-1].inspect
+
     # Skip first occurrence since it will be at the project's start date
     schedule.occurrences_between(project.starts_on, project.ends_on)[1..-1]
   end
