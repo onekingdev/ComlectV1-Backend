538cd5b604a72fab24fa11208370174f88ae2e64
Mark Tran <mark.tran@gmail.com>
2017-06-27 14:15:32 +0700

    change debugging output

--- app/models/payment_cycle.rb
+++ app/models/payment_cycle.rb
@@ -108,13 +108,13 @@ class PaymentCycle
 
   def current_cycle_date
     all_occurrences = occurrences
+    Rails.logger.info '2' * 80
+    Rails.logger.info all_occurrences.inspect
     all_occurrences.detect(&:future?) || all_occurrences.last
   end
 
   def previous_cycle_date
     all_occurrences = occurrences
-    Rails.logger.info '2' * 80
-    Rails.logger.info all_occurrences.inspect
     all_occurrences.reverse.detect(&:past?) || all_occurrences.first
   end
 
