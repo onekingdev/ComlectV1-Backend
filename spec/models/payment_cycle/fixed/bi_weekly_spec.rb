5636ec8e892be6343df2c1e174279a23ec91baa7
Mark Tran <mark.tran@gmail.com>
2017-07-06 19:40:40 +0700

    update fixed biweekly specs

--- spec/models/payment_cycle/fixed/bi_weekly_spec.rb
+++ spec/models/payment_cycle/fixed/bi_weekly_spec.rb
@@ -23,60 +23,86 @@ RSpec.describe PaymentCycle::Fixed::BiWeekly, type: :model do
     end
 
     it 'creates estimated charges every other week' do
-      JobApplication::Accept.(@job_application)
-      PaymentCycle.for(@project).reschedule!
-      expect(@project.charges.estimated.count).to eq(6)
-      expected_dates = [
-        @project.business.tz.local(2016, 1, 18, 0, 1), # Not the 15th since that's a friday so we charge on monday
-        @project.business.tz.local(2016, 2, 1, 0, 1),  # and so on...
-        @project.business.tz.local(2016, 2, 15, 0, 1),
-        @project.business.tz.local(2016, 2, 29, 0, 1),
-        @project.business.tz.local(2016, 3, 14, 0, 1),
-        @project.business.tz.local(2016, 3, 25, 0, 1)
-      ]
-      dates = @project.charges.estimated.pluck(:process_after).sort.map do |date|
-        date.in_time_zone(@project.business.tz)
+      Timecop.freeze(@project.starts_on) do
+        JobApplication::Accept.(@job_application)
+        PaymentCycle.for(@project).reschedule!
+
+        expect(@project.charges.estimated.count).to eq(6)
+
+        expected_dates = [
+          @project.business.tz.local(2016, 1, 15, 0, 1),
+          @project.business.tz.local(2016, 1, 29, 0, 1),
+          @project.business.tz.local(2016, 2, 12, 0, 1),
+          @project.business.tz.local(2016, 2, 26, 0, 1),
+          @project.business.tz.local(2016, 3, 11, 0, 1),
+          @project.business.tz.local(2016, 3, 24, 0, 1)
+        ]
+
+        dates = @project.charges.pluck(:process_after).sort.map do |date|
+          date.in_time_zone(@project.business.tz)
+        end
+
+        expect(dates).to eq(expected_dates)
+        amounts = @project.charges.order(:date).pluck(:amount_in_cents)
+        expected_amounts = [166_667, 166_667, 166_667, 166_667, 166_667, 166_665]
+        expect(amounts).to eq(expected_amounts)
       end
-      expect(dates).to eq(expected_dates)
-      amounts = @project.charges.estimated.order(:date).pluck(:amount_in_cents)
-      expected_amounts = [166_667, 166_667, 166_667, 166_667, 166_667, 166_665]
-      expect(amounts).to eq(expected_amounts)
     end
 
     context 'in the middle of project with already paid work' do
-      include TimesheetSpecHelper
-
       before do
-        JobApplication::Accept.(@job_application)
-        Timecop.freeze @project.starts_on + 2.weeks + 1.day
-        log_timesheet @project, hours: 5
-        PaymentCycle.for(@project).create_charges_and_reschedule!
-      end
+        Timecop.freeze(@project.starts_on) do
+          JobApplication::Accept.(@job_application)
+        end
 
-      after do
-        Timecop.return
+        Timecop.freeze(@project.starts_on + 2.weeks + 1.day) do
+          PaymentCycle.for(@project).create_charges_and_reschedule!
+        end
       end
 
       it 'creates first real charge two weeks after start' do
         expect(@project.charges.real.count).to eq(1)
         charge = @project.charges.real.first
         expect(charge.amount).to eq(1666.67)
-        expect(charge.process_after).to eq(@project.business.tz.local(2016, 1, 18, 0, 1))
+        expect(charge.process_after).to eq(@project.business.tz.local(2016, 1, 15, 0, 1))
       end
 
       it 'creates remaining estimated charges' do
         expect(@project.charges.estimated.count).to eq(5)
+
         expected_dates = [
-          @project.business.tz.local(2016, 2, 1, 0, 1),
-          @project.business.tz.local(2016, 2, 15, 0, 1),
-          @project.business.tz.local(2016, 2, 29, 0, 1),
-          @project.business.tz.local(2016, 3, 14, 0, 1),
-          @project.business.tz.local(2016, 3, 25, 0, 1)
+          @project.business.tz.local(2016, 1, 29, 0, 1),
+          @project.business.tz.local(2016, 2, 12, 0, 1),
+          @project.business.tz.local(2016, 2, 26, 0, 1),
+          @project.business.tz.local(2016, 3, 11, 0, 1),
+          @project.business.tz.local(2016, 3, 24, 0, 1)
         ]
+
         dates = @project.charges.estimated.pluck(:process_after).sort.map do |date|
           date.in_time_zone(@project.business.tz)
         end
+
         expect(dates).to eq(expected_dates)
+      end
+    end
+
+    context 'when project ends' do
+      before do
+        Timecop.freeze(@project.starts_on) do
+          JobApplication::Accept.(@job_application)
+        end
+
+        Timecop.freeze(@project.ends_on + 1.day) do
+          PaymentCycle.for(@project).create_charges_and_reschedule!
+        end
+
+        Timecop.freeze(@project.ends_on + 1.day + 30.minutes) do
+          ProcessScheduledChargesJob.new.perform
+        end
+      end
+
+      it 'creates a scheduled charge and processes it' do
+        expect(@project.charges.processed.size).to eq(1)
       end
     end
   end
