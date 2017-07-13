b79854d068837656467420c4137f4b8952d8bbab
Mark Tran <mark.tran@gmail.com>
2017-07-13 22:30:05 +0700

    remove params key

--- spec/requests/specialists/delete_account_spec.rb
+++ spec/requests/specialists/delete_account_spec.rb
@@ -16,7 +16,12 @@ RSpec.describe 'Deleting account', type: :request do
       end
 
       it 'cannot login anymore' do
-        post user_session_path, params: { 'user[email]' => specialist.user.email, 'user[password]' => 'password' }
+        post(
+          user_session_path,
+          'user[email]' => specialist.user.email,
+          'user[password]' => 'password'
+        )
+
         expect(response).to have_http_status(:ok)
         expect(response.body).to match(/Invalid email or password/i)
       end
