2bff397d093e8d347480b29fafcb5b61dd99b0a6
Mark Tran <mark.tran@gmail.com>
2017-07-20 23:05:27 +0700

    remove tests

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -17,47 +17,4 @@ class ProjectTest < ActiveSupport::TestCase
     project = create :project_one_off_fixed, fixed_budget: 5_000
     assert_equal 5_000, project.reload.calculated_budget
   end
-
-  test 'find project with pending business rating' do
-    business = create(:business)
-    specialist = create(:specialist)
-
-    create(
-      :project_one_off_fixed,
-      business: business,
-      fixed_budget: 5_000
-    )
-
-    project = create(
-      :project_one_off_fixed,
-      business: business,
-      specialist: specialist,
-      fixed_budget: 5_000
-    )
-
-    project_2 = create(
-      :project_one_off_fixed,
-      business: business,
-      specialist: specialist,
-      fixed_budget: 5_000
-    )
-
-    project.complete!
-    project_2.complete!
-    project_2.ratings.create!(rater: business, value: 4)
-
-    assert_equal [project.id], business.projects.pending_business_rating.pluck(:id)
-  end
-
-  test 'find project with pending specialist rating' do
-    business = create :business
-    specialist = create :specialist
-    create :project_one_off_fixed, business: business, fixed_budget: 5_000, specialist: specialist
-    project = create :project_one_off_fixed, business: business, fixed_budget: 5_000, specialist: specialist
-    project_2 = create :project_one_off_fixed, business: business, fixed_budget: 5_000, specialist: specialist
-    project.complete!
-    project_2.complete!
-    project_2.ratings.create! rater: specialist, value: 4
-    assert_equal [project.id], specialist.projects.pending_specialist_rating.pluck(:id)
-  end
 end
