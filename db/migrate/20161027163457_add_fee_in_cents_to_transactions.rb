2cac34d5af45a1e47714f665efe7f634f2466c33
Mark Tran <mark.tran@gmail.com>
2017-07-27 18:31:40 +0700

    remove data update in migration

--- db/migrate/20161027163457_add_fee_in_cents_to_transactions.rb
+++ db/migrate/20161027163457_add_fee_in_cents_to_transactions.rb
@@ -4,12 +4,5 @@ class AddFeeInCentsToTransactions < ActiveRecord::Migration
   def change
     add_column :transactions, :fee_in_cents, :integer
     add_index :transactions, :fee_in_cents
-    reversible do |dir|
-      dir.up do
-        # Set previous transactions to the old values
-        Transaction::BusinessCharge.update_all 'fee_in_cents = amount_in_cents * 0.15'
-        Transaction::SpecialistPayment.update_all 'fee_in_cents = 0'
-      end
-    end
   end
 end
