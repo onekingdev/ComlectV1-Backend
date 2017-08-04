3db4e39b3645e9eb993ab4c970108e7df2d7b352
Mark Tran <mark.tran@gmail.com>
2017-08-04 19:07:38 +0700

    add staging to bugsnag notifications

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Bugsnag.configure do |config|
-  config.notify_release_stages = 'production'
+  config.notify_release_stages = %w[staging production]
   config.release_stage = ENV['BUGSNAG_RELEASE_STAGE'] || ENV['RAILS_ENV']
 end
