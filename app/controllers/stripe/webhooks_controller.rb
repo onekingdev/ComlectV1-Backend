ec9f57e63d1250b5bb73261b28047db88c7df004
Mark Tran <mark.tran@gmail.com>
2017-08-04 13:49:20 +0700

    style updates

--- app/controllers/stripe/webhooks_controller.rb
+++ app/controllers/stripe/webhooks_controller.rb
@@ -4,7 +4,12 @@ class Stripe::WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    StripeEventJob.perform_later params[:id], params[:user_id], connect: params[:connect] == '1'
+    StripeEventJob.perform_later(
+      params[:id],
+      params[:user_id],
+      connect: params[:connect] == '1'
+    )
+
     render nothing: true, status: :ok
   end
 end
