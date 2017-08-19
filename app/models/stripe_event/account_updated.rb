26a6c079010e4c7ae28a4838b9ef88bdbe398539
Mark Tran <mark.tran@gmail.com>
2017-08-18 22:14:31 -0400

    check status attribute

--- app/models/stripe_event/account_updated.rb
+++ app/models/stripe_event/account_updated.rb
@@ -3,7 +3,7 @@
 class StripeEvent::AccountUpdated < StripeEvent
   def handle
     object = event.data.object
-    return if object.verification.fields_needed.empty?
+    return if object.verification.status == 'verified'
     stripe_account = Stripe::Account.retrieve(object.id)
     account = StripeAccount.find_by!(stripe_id: object.id)
     account.update_status_from_stripe stripe_account
