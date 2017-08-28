60cc3bcdcb480decbd8f12a494ddf57a1430de2a
Mark Tran <mark.tran@gmail.com>
2017-08-27 23:31:06 -0500

    add .legal_entity

--- app/models/stripe_event/account_updated.rb
+++ app/models/stripe_event/account_updated.rb
@@ -3,7 +3,7 @@
 class StripeEvent::AccountUpdated < StripeEvent
   def handle
     object = event.data.object
-    return if object.verification.status == 'verified'
+    return if object.legal_entity.verification.status == 'verified'
     stripe_account = Stripe::Account.retrieve(object.id)
     account = StripeAccount.find_by!(stripe_id: object.id)
     account.update_status_from_stripe stripe_account
