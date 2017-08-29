0e5248643383c19e17b154d77ef246793015baa5
Mark Tran <mark.tran@gmail.com>
2017-08-29 17:19:05 -0500

    only send notification if status is unverified

--- app/models/stripe_event/account_updated.rb
+++ app/models/stripe_event/account_updated.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/AbcSize
 class StripeEvent::AccountUpdated < StripeEvent
   def handle
     object = event.data.object
-    return if object.legal_entity.verification.status == 'verified'
-    return if object.legal_entity.verification.document.nil?
     stripe_account = Stripe::Account.retrieve(object.id)
     account = StripeAccount.find_by!(stripe_id: object.id)
     account.update_status_from_stripe stripe_account
+
+    return unless object.legal_entity.verification.status == 'unverified'
     Notification::Deliver.payment_issue! account.specialist.user
   end
 end
-# rubocop:enable Metrics/AbcSize
