c495293857348f7f240ba06a005732c87f75bd84
Mark Tran <mark.tran@gmail.com>
2017-08-28 21:43:56 -0500

    return if no document exists

--- app/models/stripe_event/account_updated.rb
+++ app/models/stripe_event/account_updated.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
 class StripeEvent::AccountUpdated < StripeEvent
   def handle
     object = event.data.object
     return if object.legal_entity.verification.status == 'verified'
+    return if object.legal_entity.verification.document.nil?
     stripe_account = Stripe::Account.retrieve(object.id)
     account = StripeAccount.find_by!(stripe_id: object.id)
     account.update_status_from_stripe stripe_account
     Notification::Deliver.payment_issue! account.specialist.user
   end
 end
+# rubocop:enable Metrics/AbcSize
