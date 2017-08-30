074c9fcd9fd8a0c566c636fb24db5fb9b36a3cd9
Mark Tran <mark.tran@gmail.com>
2017-08-29 21:51:37 -0500

    filter dob/ssn from logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password password_confirmation]
+Rails.application.config.filter_parameters += %i[dob password password_confirmation personal_id_number]
 # To not strain logs with big base64 outputs
 Rails.application.config.filter_parameters += %i[verification_document_data verification_document_cache]
