3dcb945a7e63754e0f29e15c298c3404c7991177
Mark Tran <mark.tran@gmail.com>
2018-02-11 23:32:08 +0700

    fix ordering

--- app/models/project.rb
+++ app/models/project.rb
@@ -38,7 +38,7 @@ class Project < ApplicationRecord
   scope :escalated, -> { joins(:issues).where(project_issues: { status: :open }) }
   scope :not_escalated, -> { where.not(id: escalated) }
   scope :visible, -> { joins(business: :user).where(users: { suspended: false }) }
-  scope :recent, -> { order(starts_on: :desc) }
+  scope :recent, -> { order(starts_on: :asc) }
   scope :draft_and_in_review, -> { where(status: [statuses[:draft], statuses[:review]]) }
   scope :published, -> { where(status: statuses[:published]) }
   scope :pending, -> { published.where(specialist_id: nil) }
