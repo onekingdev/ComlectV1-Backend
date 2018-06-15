9a60964715506f2f3e69859795d755ee41f10dd5
Mark Tran <mark.tran@gmail.com>
2018-06-15 18:26:51 +0900

    add BusinessMailer with new welcome message

---
+++ app/mailers/business_mailer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class BusinessMailer < ApplicationMailer
+  def welcome(business)
+    @business = business
+
+    mail(
+      to: @business.user.email,
+      template_id: ENV.fetch('POSTMARK_TEMPLATE_ID'),
+      template_model: {
+        subject: 'Welcome to Complect!',
+        message_html: render_to_string(template: 'business_mailer/welcome')
+      }
+    )
+  end
+end
