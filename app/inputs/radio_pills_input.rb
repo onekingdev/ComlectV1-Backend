3032fa5d61f29a8d93539fc29134fddca295d384
DDoS King <kourindouhime@gmail.com>
2018-08-04 10:18:18 +0300

    Static turnkey solutions

--- app/inputs/radio_pills_input.rb
+++ app/inputs/radio_pills_input.rb
@@ -7,21 +7,22 @@ class RadioPillsInput < SimpleForm::Inputs::CollectionRadioButtonsInput
 
   def input(_wrapper_options)
     template.content_tag :div, class: 'btn-group', data: { toggle: 'buttons' } do
-      if collection[0].is_a?(String) && !collection.empty?
-        collection2 = collection.map(&proc { |x| [].push(x).push(false) })
-        collection2[0][1] = true
-        collection = collection2
+      if collection[0].is_a?(String)
+        collection.each_with_index.map do |label, i|
+          each_input label, i.zero?
+        end.join.html_safe
+      else
+        collection.map do |label, value|
+          each_input label, value
+        end.join.html_safe
       end
-      collection.map do |label, value|
-        each_input label, value
-      end.join.html_safe
     end
   end
 
   private
 
   def each_input(label, value)
-    checked = object.public_send(attribute_name) == value if object
+    checked = object ? object.public_send(attribute_name) == value : value
     active_class = checked ? 'active' : nil
     template.content_tag :label, class: "btn #{btn_class} #{active_class}" do
       @builder.radio_button(attribute_name, value, input_html_options) + label.to_s
