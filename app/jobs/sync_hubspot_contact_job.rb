b88a158281c2dc86883018c80340f82da8ef8238
Mark Tran <mark.tran@gmail.com>
2018-08-27 13:37:15 -0500

    add process_all to job

--- app/jobs/sync_hubspot_contact_job.rb
+++ app/jobs/sync_hubspot_contact_job.rb
@@ -3,7 +3,15 @@
 class SyncHubspotContactJob < ApplicationJob
   queue_as :default
 
-  def perform(record)
+  def perform(record = nil)
+    return process_all if record.nil?
     HubspotContact.for(record).sync
+  end
+
+  private
+
+  def process_all
+    Business.all.each { |business| self.class.perform_later(business) }
+    Specialist.all.each { |specialist| self.class.perform_later(specialist) }
   end
 end
