df93ee56d08596d5555bb5e6865a12b3655dbd9d
Mark Tran <mark.tran@gmail.com>
2018-08-27 02:17:26 -0500

    remove sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
-# https://devcenter.heroku.com/articles/forked-pg-connections#sidekiq
-Sidekiq.configure_server do |config|
-  if (database_url = ENV['DATABASE_URL'])
-    ENV['DATABASE_URL'] = "#{database_url}?pool=#{ENV['SIDEKIQ_CONCURRENCY'] || 25}"
-    ActiveRecord::Base.establish_connection
-  end
-
-  config.redis = { size: (ENV['REDIS_CONNECTIONS'] || 27).to_i }
-end
-
-Sidekiq.configure_client do |config|
-  # 1 connection per Rails process
-  config.redis = { size: (ENV['REDIS_CLIENT_CONNECTIONS'] || 1).to_i }
-end
-
 schedule_file = Rails.root.join('config/schedule.yml')
 if File.exist?(schedule_file) && Sidekiq.server?
   schedule = YAML.load_file(schedule_file)
