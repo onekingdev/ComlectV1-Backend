bdd1fc60a5bc6d9a385b1f25bb733bdfb3b48878
DDoS King <kourindouhime@gmail.com>
2018-09-14 20:30:20 +0300

    Fix letter

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Recaptcha.configure do |config|
-  config.site_key = ENV.fetch('RECAPtCHA_SITE_KEY')
+  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY')
   config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY')
 end
