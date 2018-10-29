746967380996f1cb90fc8c9b53a79048ed65eaf0
Mark Tran <mark.tran@gmail.com>
2018-10-28 21:47:08 -0500

    allow creating token from activeadmin

--- app/admin/referral_token.rb
+++ app/admin/referral_token.rb
@@ -3,7 +3,7 @@
 ActiveAdmin.register ReferralToken, as: 'Tokens' do
   menu parent: 'Referrals'
 
-  actions :all, except: %i[new create destroy]
+  actions :all, except: %i[create destroy]
 
   permit_params :amount_in_cents, :token
 
@@ -11,6 +11,8 @@ ActiveAdmin.register ReferralToken, as: 'Tokens' do
     f.inputs do
       f.input :token
       f.input :amount_in_cents
+      f.input :referrer_id, label: 'Referrer Id'
+      f.input :referrer_type, collection: %w[Business Specialist], include_blank: false
     end
 
     f.actions
