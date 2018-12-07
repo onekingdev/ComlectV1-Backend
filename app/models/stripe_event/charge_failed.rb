3bad6bbc8de1f92cced22e1ee57d6235787b59dd
pwvcomplect <pwvcomplect@gmail.com>
2018-12-07 10:42:58 -0500

    handle failure and disable/enable accounts

--- app/models/stripe_event/charge_failed.rb
+++ app/models/stripe_event/charge_failed.rb
@@ -3,8 +3,14 @@
 class StripeEvent::ChargeFailed < StripeEvent
   def handle
     transaction = Transaction.find_by!(stripe_id: event.data.object.id)
-    transaction.error!
-    transaction.update_attribute :status_detail, event.data.object.failure_message
-    Notification::Deliver.payment_issue! transaction.business.user
+    subscription = SubscriptionCharge.find_by!(stripe_charge_id: event.data.object.id)
+    if transaction
+      transaction.error!
+      transaction.update_attribute :status_detail, event.data.object.failure_message
+      Notification::Deliver.payment_issue! transaction.business.user
+    elsif subscription
+      subscription.forum_subscription.update_attribute(suspended: true)
+      subscription.forum_subscription.business.update_attribute(qna_lvl: 0)
+    end
   end
 end
--- app/models/stripe_event/invoice_succeeded.rb
+++ app/models/stripe_event/invoice_succeeded.rb
@@ -4,7 +4,7 @@ class StripeEvent::InvoiceSucceeded < StripeEvent
   def handle
     sub = SubscriptionCharge.find_by(stripe_charge_id: event.data.object.charge)
     fs = ForumSubscription.find_by(stripe_subscription_id: event.data.object.lines.data[0].id)
-    # NOT ADDING FORUM Subscription
     sub&.update(forum_subscription: fs, status: 1)
+    fs.business.update_attribute(qna_lvl: fs[:level])
   end
 end
