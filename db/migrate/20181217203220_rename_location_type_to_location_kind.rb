2c7c13cbc5ff5a878dc69cb4eb0d24835df4fcbd
DDoS King <kourindouhime@gmail.com>
2018-12-17 23:45:49 +0300

    remove

--- db/migrate/20181217203220_rename_location_type_to_location_kind.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameLocationTypeToLocationKind < ActiveRecord::Migration
-  def change
-    rename_column :projects, :location_type, :location_kind
-  end
-end
