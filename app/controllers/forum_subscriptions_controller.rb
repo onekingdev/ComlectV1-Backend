07f03b8345c9fd1d1ce753f78bbd45b5a56c80b7
pwvcomplect <pwvcomplect@gmail.com>
2019-01-18 15:34:34 -0500

    tidies forum subscription controller

--- app/controllers/forum_subscriptions_controller.rb
+++ app/controllers/forum_subscriptions_controller.rb
@@ -7,27 +7,25 @@ class ForumSubscriptionsController < ApplicationController
     referer = URI(request.referer).path
     lvl = params[:subscription][:lvl].to_i
     billing_type = params[:billing_type].to_i
-    if [1, 2].include? lvl
-      if current_business&.payment_sources&.any? && !current_business.subscription?
-        # current_business.update(qna_lvl: lvl)
-        # rescue this
-        ForumSubscription.create(business: current_business, level: lvl, billing_type: billing_type)
-        redirect_to referer, notice: 'Subscription confirmed'
-      elsif current_business&.payment_sources&.any? && current_business.subscription?
-        # current_business.update(qna_lvl: params[:lvl])
-        current_business.forum_subscription.upgrade(lvl, billing_type)
-        redirect_to referer, notice: 'Subscription confirmed'
+    if lvl.positive?
+      if current_business&.payment_sources&.any?
+        if !current_business.subscription?
+          ForumSubscription.create(business: current_business, level: lvl, billing_type: billing_type)
+        else
+          current_business.forum_subscription.upgrade(lvl, billing_type)
+        end
+        notice = 'Subscription Confirmed'
       else
-        redirect_to referer, notice: 'Please submit a payment option to proceed'
+        notice = 'Please submit a payment option to proceed'
       end
     else
-      redirect_to referer, notice: 'Incorrect options'
+      notice = 'Incorrect options'
     end
+    redirect_to referer, notice: notice
   end
 
   def upgrade
     referer = URI(request.referer).path
-    # current_business.update(qna_lvl: params[:lvl])
     sub = ForumSubscription.where(business_id: current_business.id).first
     sub.upgrade(params[:lvl].to_i, params[:billing_type].to_i)
     redirect_to referer, notice: 'Subscription confirmed'
