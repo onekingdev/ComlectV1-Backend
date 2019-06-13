ec965aeed40046cb5369803ee805c12c95043aba
DDoS King <birb@birbs-iMac.local>
2019-06-13 21:33:05 +0300

    Notify on charge failure and fix subscriptions

--- app/models/stripe_event/charge_failed.rb
+++ app/models/stripe_event/charge_failed.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
 class StripeEvent::ChargeFailed < StripeEvent
   def handle
     payment_profile = PaymentProfile.find_by(stripe_customer_id: event.data.object.customer)
-    payment_profile.update(failed: true)
-    transaction = Transaction.find_by!(stripe_id: event.data.object.id)
-    subscription = SubscriptionCharge.find_by!(stripe_charge_id: event.data.object.id)
-    if transaction
+    unless payment_profile.failed
+      payment_profile.update(failed: true)
+      Notification::Deliver.payment_issue! payment_profile.business.user
+    end
+    transaction = Transaction.where(stripe_id: event.data.object.id)
+    subscription = SubscriptionCharge.where(stripe_customer_id: event.data.object.id)
+    # transaction = Transaction.find_by!(stripe_id: event.data.object.id)
+    # subscription = SubscriptionCharge.find_by!(stripe_charge_id: event.data.object.id)
+    if transaction.count.positive?
       transaction.error!
       transaction.update_attribute :status_detail, event.data.object.failure_message
-      Notification::Deliver.payment_issue! transaction.business.user
-    elsif subscription
+    elsif subscription.count.positive?
       subscription.failed!
       subscription.forum_subscription.update_attribute(suspended: true)
       # subscription.forum_subscription.business.update_attribute(qna_lvl: 0)
     end
   end
 end
+# rubocop:enable Metrics/AbcSize
--- app/models/stripe_event/subscription_created.rb
+++ app/models/stripe_event/subscription_created.rb
@@ -6,6 +6,6 @@ class StripeEvent::SubscriptionCreated < StripeEvent
     end_date = Time.at(event.data.object.current_period_end.to_i).utc
     subscription_id = event.data.object.id
     sub = ForumSubscription.find_by(stripe_customer_id: cust_id)
-    sub&.update(stripe_subscription_id: subscription_id, renewal_date: end_date)
+    sub&.update(stripe_subscription_id: subscription_id, renewal_date: end_date, cancelled: false, suspended: false)
   end
 end
