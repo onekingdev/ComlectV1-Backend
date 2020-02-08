aea584cc399bffab5153dfe1cb29bf118cea686e
DDoS King <birb@birbs-iMac.local>
2020-02-08 21:54:40 +0300

    no postgis for semaphore

--- db/migrate/20160802202615_enable_postgis.rb
+++ db/migrate/20160802202615_enable_postgis.rb
@@ -3,7 +3,7 @@
 class EnablePostgis < ActiveRecord::Migration
   # rubocop:disable Lint/HandleExceptions
   def change
-    enable_extension 'postgis' if Rails.env.test?
+    enable_extension 'postgis' unless Rails.env.test?
   rescue PG::UniqueViolation
   end
   # rubocop:enable Lint/HandleExceptions
