3d84f31e2e7d57d913e7107fe8817aa875a728ec
DDoS King <birb@birbs-iMac.local>
2020-03-16 23:50:58 +0300

    pdf path in production/staging

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -10,12 +10,16 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
+# rubocop:disable Metrics/LineLength
+wkhtml_bin_path = Rails.env.production? || Rails.env.staging? ? '/usr/bin/wkhtmltopdf' : Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+# rubocop:enable Metrics/LineLength
+
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: wkhtml_bin_path
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
