50ff848715c0ff92f7d0dc0bec86f9ed6c4184b0
Mike Kolganov <mike.kolganov@gmail.com>
2020-08-09 22:22:42 +0600

    Heroku app names updated

--- .semaphore/docker-push-if-needed.sh
+++ .semaphore/docker-push-if-needed.sh
@@ -5,19 +5,19 @@ case "$SEMAPHORE_GIT_BRANCH" in
   staging)
     echo "${0##*/}: Pushing Docker images for Staging"
     heroku container:login
-    docker tag complect_web registry.heroku.com/semaphore-complect-staging/web
-    docker tag complect_worker registry.heroku.com/semaphore-complect-staging/worker
-    docker push registry.heroku.com/semaphore-complect-staging/web
-    docker push registry.heroku.com/semaphore-complect-staging/worker
+    docker tag complect_web registry.heroku.com/complect-docker-staging/web
+    docker tag complect_worker registry.heroku.com/complect-docker-staging/worker
+    docker push registry.heroku.com/complect-docker-staging/web
+    docker push registry.heroku.com/complect-docker-staging/worker
     ;;
 
   master)
     echo "${0##*/}: Pushing Docker images for Production"
     heroku container:login
-    docker tag complect_web registry.heroku.com/docker-production/web
-    docker tag complect_worker registry.heroku.com/docker-production/worker
-    docker push registry.heroku.com/docker-production/web
-    docker push registry.heroku.com/docker-production/worker
+    docker tag complect_web registry.heroku.com/complect-docker-production/web
+    docker tag complect_worker registry.heroku.com/complect-docker-production/worker
+    docker push registry.heroku.com/complect-docker-production/web
+    docker push registry.heroku.com/complect-docker-production/worker
     ;;
 
   *)
