2cac1817a3b556f015c799f73db6e63e90e96d65
Mahabubul Alam <zikoku07@gmail.com>
2020-09-23 16:40:59 +0600

    create internal department

--- app/models/project_end.rb
+++ app/models/project_end.rb
@@ -34,9 +34,12 @@ class ProjectEnd < ApplicationRecord
       :ends_on,
       Time.zone.now.in_time_zone(project.business.tz)
     )
-
-    reset_upcoming_charges
-    project.fixed_pricing? ? trigger_fixed_project_end : trigger_hourly_project_end
+    if project.internal?
+      project.complete!
+    else
+      reset_upcoming_charges
+      project.fixed_pricing? ? trigger_fixed_project_end : trigger_hourly_project_end
+    end
   end
 
   def trigger_fixed_project_end
