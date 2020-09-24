0c996e326a5bfb7e5e3880b70171e78a64878489
kourindouhime <625899+kourindouhime@users.noreply.github.com>
2020-09-24 18:53:55 +0300

    Remindermailer (#301)
    * Pass id and type instead of object to job
    
    * fix beginning date
    
    * debug
    
    Co-authored-by: DDoS King <birb@birbs-iMac.local>

--- app/jobs/reminder_mailer_job.rb
+++ app/jobs/reminder_mailer_job.rb
@@ -3,16 +3,25 @@
 class ReminderMailerJob < ApplicationJob
   include RemindersFetcher
   queue_as :mailers
-
+  # rubocop:disable Rails/Output
+  # rubocop:disable Style/GuardClause
   def perform(remindable = nil)
     return process_all if remindable.nil?
+    puts "AYYYYYY PROCESSING #{remindable}"
     remindable.update(reminders_mailed_at: Time.zone.today.in_time_zone(remindable.time_zone).to_date)
     calendar_grid = tasks_calendar_grid(remindable, Time.zone.today.in_time_zone(remindable.time_zone).beginning_of_month
     .to_date)
     todays = reminders_today(remindable, calendar_grid)
     past_dues = reminders_past(remindable)
-    ReminderMailer.send_today(remindable, past_dues, todays) if (todays.length + past_dues.length).positive?
+    puts todays.length
+    puts past_dues.length
+    if (todays.length + past_dues.length).positive?
+      ReminderMailer.send_today(remindable, past_dues, todays)
+      puts 'Mail sent'
+    end
   end
+  # rubocop:enable Style/GuardClause
+  # rubocop:enable Rails/Output
 
   private
 
