76f9a642506bd45a77ce805e2ceb4c466df05f26
7audio <7audio>
2021-01-03 20:10:05 +0300

    Add Vue asset build

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker')
 
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path')
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+
+environment.loaders.prepend('pug', {
+    test: /\.pug$/,
+    use: [{
+        loader: 'pug-plain-loader'
+    }]
+})
+
 module.exports = environment
