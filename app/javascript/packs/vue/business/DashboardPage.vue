249635ec1f13b2b0b29d7216151aa3358e51c4c9
7audio <7audio>
2021-01-09 21:24:00 +0300

    Business Dashboard: add Task status colors in calendar

--- app/javascript/packs/vue/business/DashboardPage.vue
+++ app/javascript/packs/vue/business/DashboardPage.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <FullCalendar :options="calendarOptions"/>
+  <div class="row">
+    <div class="col-md-7 col-sm-12">
+      <FullCalendar :options="calendarOptions"/>
+    </div>
+    <div class="col-md-5 col-sm-12">
+      <h3>Bar colors</h3>
+      <dl>
+        <dt><span class="task-is-task">✅</span></dt>
+        <dd>Tasks</dd>
+        <dt><span class="task-is-project">📄</span></dt>
+        <dd>Projects</dd>
+        <dt><span class="task-is-overdue">📄</span></dt>
+        <dd>Overdue</dd>
+        <dt><span class="task-is-complete">📄</span></dt>
+        <dd>Complete</dd>
+      </dl>
+    </div>
   </div>
 </template>
 
@@ -16,11 +31,18 @@ const toEvent = task => ({
   start: task.starts_on || task.remind_at,
   end: task.ends_on,
   title: task.title || task.body,
-  classNames: [isProject(task) ? 'task-is-project' : isTask(task) ? 'task-is-task' : '']
+  classNames: [
+    isComplete(task) ? 'task-is-complete'
+    : isOverdue(task) ? 'task-is-overdue'
+    : isProject(task) ? 'task-is-project'
+    : isTask(task) ? 'task-is-task' : ''
+  ]
 })
 
 const isProject = task => task.hasOwnProperty('only_regulators') && task.hasOwnProperty('key_deliverables')
 const isTask = task => task.hasOwnProperty('remindable_type') && task.hasOwnProperty('skip_occurencies')
+const isOverdue = task => DateTime.fromISO(task.starts_on || task.remind_at) <= DateTime.local()
+const isComplete = task => task.completed_at || task.done_at
 
 export default {
   computed: {
@@ -56,6 +78,20 @@ export default {
   background-color: var(--bs-green) !important;
 }
 .task-is-task .fc-event-title:before {
+  content: '✅';
+}
+.task-is-overdue,
+.task-is-overdue .fc-event-title {
+  background-color: var(--bs-warning) !important;
+}
+.task-is-overdue .fc-event-title:before {
+  content: '⚠️';
+}
+.task-is-complete,
+.task-is-complete .fc-event-title {
+  background-color: var(--bs-gray) !important;
+}
+.task-is-complete .fc-event-title:before {
   content: '✅';
 }
 </style>
