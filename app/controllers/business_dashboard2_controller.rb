d5e8e0198ccc6f483ec54bcebfdeafdcd320f5c4
7audio <7audio>
2021-01-09 19:55:47 +0300

    Remove redundant action

--- app/controllers/business_dashboard2_controller.rb
+++ app/controllers/business_dashboard2_controller.rb
@@ -9,35 +9,6 @@ class BusinessDashboard2Controller < ApplicationController
   before_action :beginning_of_week
 
   def show
-    @range_axis = []
-    @financials = Business::Financials.for(current_business)
-    @business = current_user.business
-    @compliance_spend = ['Spent'] + transactions_monthly(@business)
-    @seats_total = current_business.seats.count
-    @seats_available = current_business.seats.available.count
-    @seats_taken = @seats_total - @seats_available
-    @business.spawn_compliance_policies
-    if current_business.ria_dashboard?
-      assigned_team_members_ids = current_business.seats.pluck(:team_member_id).compact
-      @assigned_team_members = TeamMember.where(id: assigned_team_members_ids)
-    end
-    @financials = Business::Financials.for(current_business)
-    @ratings = @business.ratings_received.preload_association
-    @reminders_past = reminders_past(current_business)
-    # @reminders_today = reminders_today(current_business, @calendar_grid)
-    # @reminders_week = reminders_week(current_business, @calendar_grid)
-    @calendar_grid = tasks_calendar_grid(current_business, Date.parse(params[:start_date]).beginning_of_month) if params[:start_date]
-    @current_year_annual_review = @business.processed_annual_reviews.where(year: Time.zone.today.year)
-    respond_to do |format|
-      format.html do
-      end
-      format.json do
-        render json: @calendar_grid
-      end
-    end
-  end
-
-  def show_2
     render :html => '<business-dashboard-page></business-dashboard-page>'.html_safe, :layout => 'vue'
   end
 
