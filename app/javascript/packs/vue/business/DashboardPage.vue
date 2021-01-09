23974da9bf84b351dd63f3e44458fd4cad7db195
7audio <7audio>
2021-01-09 20:32:11 +0300

    Business Dashboard: render Projects and Tasks differently

--- app/javascript/packs/vue/business/DashboardPage.vue
+++ app/javascript/packs/vue/business/DashboardPage.vue
@@ -11,7 +11,16 @@ import { DateTime } from 'luxon'
 
 const endpointUrl = '/api/business/tasks/'
 const jsToSql = date => DateTime.fromJSDate(date).toSQLDate()
-const toEvent = task => ({ ...task, start: task.starts_on, end: task.ends_on })
+const toEvent = task => ({
+  ...task,
+  start: task.starts_on || task.remind_at,
+  end: task.ends_on,
+  title: task.title || task.body,
+  classNames: [isProject(task) ? 'task-is-project' : isTask(task) ? 'task-is-task' : '']
+})
+
+const isProject = task => task.hasOwnProperty('only_regulators') && task.hasOwnProperty('key_deliverables')
+const isTask = task => task.hasOwnProperty('remindable_type') && task.hasOwnProperty('skip_occurencies')
 
 export default {
   computed: {
@@ -33,3 +42,20 @@ export default {
   }
 }
 </script>
+
+<style>
+.task-is-project,
+.task-is-project .fc-event-title {
+  background-color: var(--bs-blue) !important;
+}
+.task-is-project .fc-event-title:before {
+  content: '📄';
+}
+.task-is-task,
+.task-is-task .fc-event-title {
+  background-color: var(--bs-green) !important;
+}
+.task-is-task .fc-event-title:before {
+  content: '✅';
+}
+</style>
