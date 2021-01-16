5874f309cbfe2f34ef29874d64cca6d4f203cc57
7audio <7audio>
2021-01-16 13:32:43 +0300

    Update CreateTaskModal bootstrap components

--- app/javascript/packs/vue/common/CreateTaskModal.vue
+++ app/javascript/packs/vue/common/CreateTaskModal.vue
@@ -1,48 +1,36 @@
 <template lang="pug">
   div
-    div(data-bs-toggle="modal" :data-bs-target="`#${id}`")
+    div(v-b-modal="id")
       slot
 
-    .modal.fade(:id="id")
-      .modal-dialog
-        .modal-content
-          .modal-header
-            h5.modal-title New task
-            button.btn-close(data-bs-dismiss="modal")
-          .modal-body
-            .mb-3
-                label.form-label Task name
-                input.form-control(type=text placeholder="Enter the name of your task")
+    b-modal.fade(:id="id" title="New task")
+      label.form-label Task name
+      input.form-control(type=text placeholder="Enter the name of your task")
 
-                label.form-label Link to
-                Treeselect(v-model="linkTo" :multiple="false" :options="linkToOptions" placeholder="Select projects, annual reviews, or policies to link the task to")
-                .form-text Optional
+      label.form-label Link to
+      Treeselect(v-model="linkTo" :multiple="false" :options="linkToOptions" placeholder="Select projects, annual reviews, or policies to link the task to")
+      .form-text.text-muted Optional
 
-                label.form-label Assignee
-                Treeselect(v-model="assignee" :multiple="false" :options="assigneeOptions" placeholder="Select an assignee")
+      label.form-label Assignee
+      Treeselect(v-model="assignee" :multiple="false" :options="assigneeOptions" placeholder="Select an assignee")
 
-                .container.g-0
-                  .row
-                    .col-sm
-                      label.form-label Start Date
-                      input.form-control(type=text :placeholder="dateFormat")
-                      .form-text Optional
-                    .col-sm
-                      label.form-label Due Date
-                      input.form-control(type=text :placeholder="dateFormat")
+      b-row(no-gutters)
+        .col-sm
+          label.form-label Start Date
+          b-form-datepicker(v-model="startDate" :placeholder="dateFormat")
+          .form-text.text-muted Optional
+        .col-sm
+          label.form-label Due Date
+          b-form-datepicker(v-model="dueDate" :placeholder="dateFormat")
 
-                label.form-label Repeats
-                select.form-select
-                  option(value=1 selected) Does not repeat
-                  option(value=2 selected) Weekly
-                  option(value=3 selected) Monthly
+      label.form-label Repeats
+      b-form-select(v-model="repeats" :options="['Does not repeat', 'Weekly', 'Monthly']")
 
-                label.form-label Description
-                textarea.form-control(rows=3)
-                .form-text Optional
-          .modal-footer
-            button.btn(data-bs-dismiss="modal") Cancel
-            button.btn.btn-primary Create
+      label.form-label Description
+      textarea.form-control(rows=3)
+      .form-text.text-muted Optional
+
+      //- cancel, create
 </template>
 
 <script>
@@ -55,7 +43,10 @@ export default {
     return {
       id: `modal_${rnd()}`,
       linkTo: null,
-      assignee: null
+      assignee: null,
+      startDate: null,
+      dueDate: null,
+      repeats: null
     }
   },
   computed: {
