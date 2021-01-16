4b806d22c63a076b93d410e029d1916031f07029
7audio <7audio>
2021-01-16 12:21:13 +0300

    Add Bootstrap 4 Vue to bundle

--- app/javascript/packs/vue.js
+++ app/javascript/packs/vue.js
@@ -1,8 +1,11 @@
 import Vue from 'vue/dist/vue.esm.js'
 import BusinessDashboardPage from './vue/business/dashboard/Page.vue'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Treeselect from '@riophae/vue-treeselect'
 import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
