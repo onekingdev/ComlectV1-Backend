842518bab9c0ecef49886011b6a8e70dabf7f358
7audio <7audio>
2021-01-17 11:43:12 +0300

    Fetching Projects list

--- app/javascript/packs/vue/business/dashboard/ProjectTable.vue
+++ app/javascript/packs/vue/business/dashboard/ProjectTable.vue
@@ -16,11 +16,32 @@
             th Start Date
             th End Date
         tbody
-          tr
-            td(colspan=6)
+          tr(v-for="project in projects" :key="project.id")
+            td {{ project.assignee }}
+            td {{ project.title }}
+            td {{ project.fixed_budget || project.hourly_rate }}
+            td
+              span.badge.badge-primary {{ project.status }}
+            td {{ project.starts_on }}
+            td {{ project.ends_on }}
 </template>
 
 <script>
+import { DateTime } from 'luxon'
+
+const endpointUrl = '/api/business/tasks/'
+
 export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  created() {
+    const fromTo = DateTime.local().startOf('month').toSQLDate() + '/' + DateTime.local().endOf('month').toSQLDate()
+    fetch(`${endpointUrl}${fromTo}`, { headers: {'Accept': 'application/json'}})
+      .then(response => response.json())
+      .then(result => this.projects = result.projects)
+  }
 }
 </script>
