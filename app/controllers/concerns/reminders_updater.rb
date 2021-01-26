227b7eaef2f3e7c5dafb8d2b934d646f1c41d257
DDoS King <birb@birbs-iMac.local>
2021-01-26 16:19:16 +0300

    note => nil

--- app/controllers/concerns/reminders_updater.rb
+++ app/controllers/concerns/reminders_updater.rb
@@ -6,16 +6,16 @@ module RemindersUpdater
     src_reminder.update(skip_occurencies: src_reminder.skip_occurencies + [params[:oid].to_i])
   end
 
-  def change_reminder_state(note)
+  def change_reminder_state
     upd_time = Time.zone.now.in_time_zone(@reminder.remindable.time_zone)
     if params[:oid].present?
       if params[:done] == 'false'
         @reminder.update(done_occurencies: @reminder.done_occurencies.tap { |hs| hs.delete(params[:oid].to_i) })
       else
-        @reminder.update(done_occurencies: @reminder.done_occurencies.merge(params[:oid].to_i => [upd_time, note]))
+        @reminder.update(done_occurencies: @reminder.done_occurencies.merge(params[:oid].to_i => [upd_time, nil]))
       end
     else
-      @reminder.update(done_at: (params[:done] == 'false' ? nil : upd_time), note: note)
+      @reminder.update(done_at: (params[:done] == 'false' ? nil : upd_time), note: nil)
     end
   end
 end
