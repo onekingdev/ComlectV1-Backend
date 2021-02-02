a8b43f2467793f2ae0846ba10c88afd9d42e3cbd
7audio <7audio>
2021-02-02 10:44:52 +0300

    Add WizardProgress widget

---
+++ app/javascript/packs/vue/common/WizardProgress.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  b-row(no-gutters)
+    .col-sm.bg-dark.text-light(v-for="(label, i) in stepsActive" :key="label")
+      b {{1+i}}. {{label}}
+    .col-sm(v-for="(label, i) in stepsInactive" :key="label")
+      | {{1+i}}. {{label}}
+</template>
+
+<script>
+export default {
+  props: {
+    step: {
+      type: String,
+      required: true
+    },
+    steps: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    stepsActive() {
+      return this.steps.slice(0, 1 + this.steps.indexOf(this.step))
+    },
+    stepsInactive() {
+      return this.steps.slice(1 + this.steps.indexOf(this.step))
+    }
+  }
+}
+</script>
