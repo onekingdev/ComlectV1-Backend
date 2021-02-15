3f7b151404a8231178a911a9c6314ffc38fa71f4
7audio <7audio>
2021-02-15 21:49:57 +0300

    Add LocalProject collaborators relation

--- app/models/local_project.rb
+++ app/models/local_project.rb
@@ -4,4 +4,5 @@ class LocalProject < ApplicationRecord
   has_many :projects
   belongs_to :business
   has_one :visible_project, -> { where(status: Project.statuses[:published]).order(id: :desc).limit(1) }, class_name: 'Project'
+  has_many :collaborators, source: :specialist, through: :projects, class_name: 'Specialist'
 end
