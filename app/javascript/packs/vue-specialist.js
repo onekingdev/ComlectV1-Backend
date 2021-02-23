5d572200d3bbc1a6677ce3f2015cb97030407b6f
7audio <7audio>
2021-02-23 18:04:17 +0300

    Add Specialist MyProjects page

--- app/javascript/packs/vue-specialist.js
+++ app/javascript/packs/vue-specialist.js
@@ -1,10 +1,12 @@
 import init from '@/init'
 import ProjectIndexPage from '@/specialist/projects/IndexPage'
 import CreateProposalPage from '@/specialist/projects/CreateProposalPage'
+import MyProjectsPage from '@/specialist/projects/MyProjectsPage'
 
 init({
   components: {
     ProjectIndexPage,
-    CreateProposalPage
+    CreateProposalPage,
+    MyProjectsPage
   }
 })
---
+++ app/javascript/packs/vue/specialist/projects/MyProjectsPage.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  b-tabs(content-class="mt-3")
+    b-tab(title="My Projects" active)
+      .row
+        .col-sm
+          select.custom-select
+            option(value) Filter by: All
+        .col-sm
+          select.custom-select
+            option(value) Year: All
+      table.table
+        thead
+          tr
+            th Title
+            th Client
+            th Payment
+            th Status
+            th Start Date
+            th End Date
+        tbody
+          tr(v-for="project in projects" :key="project.id")
+            td
+            td
+            td
+            td
+            td
+            td
+    b-tab(title="Contacts")
+      p Contacts
+    b-tab(title="Ratings and Reviews")
+      p Ratings and Reviews
+</template>
+
+<script>
+export default {
+  computed: {
+    projects() {
+      return []
+    }
+  }
+}
+</script>
