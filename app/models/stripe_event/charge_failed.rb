4d9f7578f74bf4d12f7a7e8d23237acc96aa2f1e
kourindouhime <625899+kourindouhime@users.noreply.github.com>
2021-02-25 00:36:20 +0300

    Add first (#361)
    Co-authored-by: DDoS King <birb@birbs-iMac.local>

--- app/models/stripe_event/charge_failed.rb
+++ app/models/stripe_event/charge_failed.rb
@@ -9,14 +9,12 @@ class StripeEvent::ChargeFailed < StripeEvent
     end
     transaction = Transaction.where(stripe_id: event.data.object.id)
     subscription = SubscriptionCharge.where(stripe_charge_id: event.data.object.id)
-    # transaction = Transaction.find_by!(stripe_id: event.data.object.id)
-    # subscription = SubscriptionCharge.find_by!(stripe_charge_id: event.data.object.id)
     if transaction.count.positive?
-      transaction.error!
-      transaction.update_attribute :status_detail, event.data.object.failure_message
+      transaction.first.error!
+      transaction.first.update_attribute :status_detail, event.data.object.failure_message
     elsif subscription.count.positive?
-      subscription.failed!
-      subscription.forum_subscription.update_attribute(suspended: true)
+      subscription.first.failed!
+      subscription.first.forum_subscription.update_attribute(suspended: true)
       # subscription.forum_subscription.business.update_attribute(qna_lvl: 0)
     end
   end
