5a0f25c3e07eaab62b2229bc770d114bc0ec5dcf
kourindouhime <625899+kourindouhime@users.noreply.github.com>
2021-02-25 05:19:05 +0300

    fix migration (#363)
    Co-authored-by: DDoS King <birb@birbs-iMac.local>

--- db/migrate/20210203192622_add_zero_fee_to_specialists.rb
+++ db/migrate/20210203192622_add_zero_fee_to_specialists.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddZeroFeeToSpecialists < ActiveRecord::Migration
+class AddZeroFeeToSpecialists < ActiveRecord::Migration[6.0]
   def change
     add_column :specialists, :zero_fee, :boolean, default: false
   end
