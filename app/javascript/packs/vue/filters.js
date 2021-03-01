ee2ef35aeec780c22f911730de687618e3821592
7audio <7audio>
2021-03-02 01:41:15 +0300

    Add minToHour filter

--- app/javascript/packs/vue/filters.js
+++ app/javascript/packs/vue/filters.js
@@ -14,6 +14,7 @@ const filters = {
   yesNo: val => val ? 'Yes' : 'No',
   isAre: items => (Array.isArray(items) ? items.length : items) === 1 ? 'is' : 'are',
   commas: arr => arr.filter(v => !!v).join(', '),
+  minToHour: min => Math.floor(min / 60) + ':' + `${min % 60}`.padStart(2, '0'),
 }
 
 export default filters
