1d1b3cb776f9150b85fded3f99503b56d0d85d1c
7audio <7audio>
2021-03-10 23:52:12 +0300

    Business: Add "Project Posted" alert

--- app/javascript/packs/vue/business/projects/ApplicationsNotice.vue
+++ app/javascript/packs/vue/business/projects/ApplicationsNotice.vue
@@ -3,7 +3,11 @@
     .alert.alert-warning(v-if="applications.length")
       h4.alert-heading {{ 'application' | plural(applications) }} received.
       p There {{ applications | isAre }} currently {{ 'applicant' | plural(applications) }} for your project.
-      a.btn.btn-light(:href="viewApplicantsUrl") View
+      a.btn.btn-light(:href="viewPostUrl") View
+    .alert.alert-info(v-else)
+      h4.alert-heading Your project is currently posted on the job board as of {{ project.created_at | asDate }}.
+      p Keep an eye out! Specialists may reach out to you soon.
+      a.btn.btn-light(:href="viewPostUrl") View
 </template>
 
 <script>
@@ -21,7 +25,7 @@ export default {
     apiUrl() {
       return this.$store.getters.url('URL_API_PROJECT_APPLICATIONS', this.project.id)
     },
-    viewApplicantsUrl() {
+    viewPostUrl() {
       return this.$store.getters.url('URL_PROJECT_POST', this.project.id)
     }
   }
