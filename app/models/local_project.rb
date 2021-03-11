e68864deecbbb0a1502d7222e86e776de85b81cf
7audio <7audio>
2021-03-11 16:17:30 +0300

    Add LocalProject.title requirement

--- app/models/local_project.rb
+++ app/models/local_project.rb
@@ -6,6 +6,8 @@ class LocalProject < ApplicationRecord
   has_one :visible_project, -> { where(status: Project.statuses[:published]).order(id: :desc).limit(1) }, class_name: 'Project'
   has_many :collaborators, source: :specialist, through: :projects, class_name: 'Specialist'
 
+  validates :title, presence: true
+
   enum status: {
     inprogress: 'inprogress',
     draft: 'draft',
