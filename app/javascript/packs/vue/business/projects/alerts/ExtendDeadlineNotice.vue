7e59271d476827e7a6a886c695854b5a82b526a3
7audio <7audio>
2021-03-26 00:42:33 +0300

    Business: Extend Deadline modal markup

--- app/javascript/packs/vue/business/projects/alerts/ExtendDeadlineNotice.vue
+++ app/javascript/packs/vue/business/projects/alerts/ExtendDeadlineNotice.vue
@@ -2,8 +2,13 @@
   .alert.alert-warning(v-if="isVisible")
     h4.alert-heading The project's due date is tomorrow.
     p
-      button.btn.btn-default.float-right(@click="extend") Extend
+      button.btn.btn-default.float-right(v-b-modal="'ExtendDeadlineModal'") Extend
       | Do you want to extend the deadline?
+      b-modal(id="ExtendDeadlineModal" title="Extend Deadline")
+        InputDate(v-model="form.ends_on" :options="datepickerOptions") New Due Date
+        template(#modal-footer="{ hide }")
+          button.btn.btn-default.float-right(@click="hide") Cancel
+          button.btn.btn-dark.float-right Confirm
 </template>
 
 <script>
@@ -16,13 +21,19 @@ export default {
       required: true
     }
   },
-  methods: {
-    extend() {
+  data() {
+    return {
+      form: { ends_on: null }
     }
   },
   computed: {
     isVisible() {
       return DateTime.local().plus({ days: 1 }).toSQLDate() === this.project.ends_on
+    },
+    datepickerOptions() {
+      return {
+        min: DateTime.local().plus({ days: 1 }).toJSDate()
+      }
     }
   }
 }
