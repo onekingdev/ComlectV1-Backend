4d19d0610558aa8528ecea7e21eb393511f77473
DDoS King <birb@birbs-iMac.local>
2021-03-31 01:14:58 +0300

    discussions fix

--- app/controllers/api/project_messages_controller.rb
+++ app/controllers/api/project_messages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::ProjectMessagesController < ApiController
-  before_action :require_someone
+  before_action :require_someone!
   before_action :find_project
 
   def index
@@ -9,13 +9,17 @@ class Api::ProjectMessagesController < ApiController
   end
 
   def create
-    message = Message::Create.(@project, message_params.merge(sender: @someone, recipient: nil), @someone, nil)
+    message = Message::Create.(@project, message_params.merge(sender: @current_someone, recipient: nil), @current_someone, nil)
     render json: message.to_json
   end
 
   private
 
   def find_project
-    @project = @someone.local_projects.find(params[:project_id])
+    @project = @current_someone.local_projects.find(params[:project_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:message, :file)
   end
 end
