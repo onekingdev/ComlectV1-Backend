c3fe8d71425adfab797c98c4a91a1e252dcd2cd9
DDoS King <birb@birbs-iMac.local>
2021-03-30 21:38:45 +0300

    enable legacy extension

--- app/models/project_extension/request.rb
+++ app/models/project_extension/request.rb
@@ -15,11 +15,11 @@ class ProjectExtension::Request < Draper::Decorator
     new(create!(project: project,
                 expires_at: expires_at,
                 ends_on: params[:ends_on],
-                starts_on: params[:starts_on],
-                role_details: params[:role_details],
-                key_deliverables: params[:key_deliverables],
-                fixed_budget: (project.fixed_budget? ? params[:fixed_budget] : nil),
-                hourly_rate: (project.hourly_rate? ? params[:hourly_rate] : nil),
+                starts_on: (params[:starts_on] || project.starts_on),
+                role_details: (params[:role_details] || project.role_details),
+                key_deliverables: (params[:key_deliverables] || project.key_deliverables),
+                fixed_budget: (project.fixed_budget? ? (params[:fixed_budget] || project.fixed_budget) : nil),
+                hourly_rate: (project.hourly_rate? ? (params[:hourly_rate] || project.hourly_rate) : nil),
                 requester: someone.class.name)).tap do |request|
       Notification::Deliver.extend_project! request
     end
