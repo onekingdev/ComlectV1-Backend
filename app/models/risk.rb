79e8c5bba7543f23d4de720ea532f868bf72ca3e
DDoS King <birb@birbs-iMac.local>
2021-04-07 00:41:01 +0300

    risk level generator

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -7,4 +7,14 @@ class Risk < ApplicationRecord
   validates :name, presence: true
   validates :impact, presence: true
   validates :likelihood, presence: true
+
+  def risk_level
+    if likelihood.positive? && impact == 2
+      2
+    elsif likelihood < 2 && impact.zero?
+      0
+    else
+      1
+    end
+  end
 end
