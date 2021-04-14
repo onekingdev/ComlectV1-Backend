50ae403062027ad147e943f9a5bef931a9d586a7
oscarhandsome <altair5555@gmail.com>
2021-04-14 19:03:07 +0600

    added page for specialist marketplace

--- app/javascript/packs/vue-business.js
+++ app/javascript/packs/vue-business.js
@@ -11,14 +11,24 @@ import TimesheetsShowPage from '@/business/projects/TimesheetsShowPage'
 
 import BusinessPoliciesPage from './vue/business/policies/Page.vue'
 import BusinessPoliciesCreatePage from './vue/business/policies/Details/PolicyCreate.vue'
-// import BusinessPoliciesCreatePageSubsection from './vue/business/policies/PolicySubsection.vue'
-// import BusinessPoliciesCreatePageHistory from './vue/business/policies/PolicyTable.vue'
 
 const businesPolicies = {
   BusinessPoliciesPage,
   BusinessPoliciesCreatePage,
-  // BusinessPoliciesCreatePageSubsection,
-  // BusinessPoliciesCreatePageHistory
+}
+
+import BusinessRisksPage from './vue/business/riskregister/Page.vue'
+import BusinessRisksPageDetail from './vue/business/riskregister/RiskDetail.vue'
+
+const businesRisks = {
+  BusinessRisksPage,
+  BusinessRisksPageDetail
+}
+
+import BusinessMarketplacePage from './vue/business/marketplace/Page.vue'
+
+const businesMarketplace = {
+  BusinessPoliciesPage,
 }
 
 init({
@@ -33,6 +43,8 @@ init({
     RemindersPage,
     TimesheetsShowPage,
 
-    ...businesPolicies
+    ...businesPolicies,
+    ...businesRisks,
+    ...businesMarketplace
   }
 })
---
+++ app/javascript/packs/vue/business/marketplace/Page.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div
+    .container
+      .row
+        .col-12
+          .row.p-x-1
+            .col-md-12.p-t-3.d-flex.justify-content-between.p-b-1
+              div
+                h2: b {{ pageTitle }}
+                p Find the right consultant for the right job
+          .row
+            .col-12 sadsd
+</template>
+
+<script>
+  export default {
+    components: {
+
+    },
+    data() {
+      return {
+        pageTitle: "Specialist Marketplace",
+      };
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+  };
+</script>
+
+<style>
+  @import "./styles.css";
+</style>
