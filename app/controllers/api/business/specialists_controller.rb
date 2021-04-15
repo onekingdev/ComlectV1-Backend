ad155db70436af0c99f0e2f7eb742a2d6b84c801
DDoS King <birb@birbs-iMac.local>
2021-04-15 20:15:35 +0300

    Dummy specialist search

--- app/controllers/api/business/specialists_controller.rb
+++ app/controllers/api/business/specialists_controller.rb
@@ -4,14 +4,18 @@ class Api::Business::SpecialistsController < ApiController
   before_action :authenticate_user!
   before_action :require_business!
 
-  FILTERS = {
-    'hired' => :hired,
-    'favorited' => :favorited
-  }.freeze
-
   def index
-    filter = FILTERS[params[:filter]] || :none
-    specialists = paginate current_business.filtered_specialists(filter)
+    # specialists = Specialist::Search.new(search_params).results
+    specialists = paginate Specialist.where(specialist_team_id: nil)
     respond_with specialists, each_serializer: ::Business::SpecialistSerializer
+  end
+
+  private
+
+  def search_params
+    params.permit(
+      :keyword, :regulator, skill_names: [],
+                            industry_ids: [], jurisdiction_ids: [], experience: [], hourly_rate_range: []
+    )
   end
 end
