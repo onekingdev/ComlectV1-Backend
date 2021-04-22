2694ccd4803c309d671b7607828990387e822edc
DDoS King <birb@birbs-iMac.local>
2021-04-22 12:03:56 +0300

    sidekiq fix and sign up props

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,7 +8,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :js, :html
 
   def new
-    render html: content_tag('business-signup-page', '').html_safe, layout: 'vue_onboarding'
+    render html: content_tag('business-signup-page', '',
+                             ':industry-ids': Industry.all.map(&proc { |ind|
+                                                                  { id: ind.id,
+                                                                    name: ind.name }
+                                                                }).to_json,
+                             ':jurisdiction-ids': Jurisdiction.all.map(&proc { |ind|
+                                                                          { id: ind.id,
+                                                                            name: ind.name }
+                                                                        }).to_json,
+                             ':sub-industry-ids': sub_industries(false).to_json,
+                             ':states': State.fetch_all_usa.to_json).html_safe,
+           layout: 'vue_onboarding'
   end
 
   # GET /resource/sign_up
