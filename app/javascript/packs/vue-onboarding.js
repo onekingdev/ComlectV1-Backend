972107570669d14e8eb1d6df85a259cedb26c79c
oscarhandsome <altair5555@gmail.com>
2021-04-27 23:08:03 +0600

    fixed names of files

--- app/javascript/packs/vue-onboarding.js
+++ app/javascript/packs/vue-onboarding.js
@@ -1,15 +1,15 @@
 import init from './vue/init'
 import store from '@/store/common'
-import BusinessSignupPage from './vue/auth/SignUp/Page.vue'
-import BusinessSigninPage from './vue/auth/SingIn/Page.vue'
+import SignupPage from './vue/auth/SignUp/Page.vue'
+import SigninPage from './vue/auth/SingIn/Page.vue'
 import SpecialistOnboardingPage from './vue/auth/SignUp/Onboarding/Specialist/SpecialistPage.vue'
 import BusinessOnboardingPage from './vue/auth/SignUp/Onboarding/Business/BusinessPage.vue'
 
 init({
   store,
   components: {
-    BusinessSignupPage,
-    BusinessSigninPage,
+    SignupPage,
+    SigninPage,
     SpecialistOnboardingPage,
     BusinessOnboardingPage
   }
