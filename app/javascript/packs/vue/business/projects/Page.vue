1f79295c27a377d273b6e1eaf8051859d7c0f8de
Tikhon Sokolov <tikhon.sokolov>
2021-05-05 20:59:31 +0300

    Fix loading business ratings URL

--- app/javascript/packs/vue/business/projects/Page.vue
+++ app/javascript/packs/vue/business/projects/Page.vue
@@ -50,7 +50,7 @@
       b-tab(title="Ratings and Reviews")
         .card-body.white-card-body
           .container
-            Get(ratings='/api/business/ratings'): template(v-slot="{ratings}"): table.rating_table
+            Get(ratings='/api/project_ratings'): template(v-slot="{ratings}"): table.rating_table
               tbody
                 tr(v-for="rating in ratings")
                   td
