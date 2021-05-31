51e120a68baaf2609158d9f432e5e2ebdbdb9781
Nikki <nikki.arnette1337@gmail.com>
2021-06-01 09:32:56 +1000

    fix

--- app/controllers/api/settings/password_controller.rb
+++ app/controllers/api/settings/password_controller.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Api::Settings::PasswordController < ApiController
+    skip_before_action :verify_authenticity_token
 
     def update
-      if @current_user.update_with_password(password_params)
-        bypass_sign_in @current_user
+      if current_user.update_with_password(password_params)
+        bypass_sign_in current_user
         respond_with status: :ok
       else
-        respond_with errors: @current_user.errors, status: :fail
+        respond_with errors: current_user.errors, status: :fail
       end
     end
 
