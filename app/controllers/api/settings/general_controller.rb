3adcc8e5cbbc05a413d50d8cd8624c80fc915c17
Nikki <nikki.arnette1337@gmail.com>
2021-06-01 09:12:33 +1000

    fix

--- app/controllers/api/settings/general_controller.rb
+++ app/controllers/api/settings/general_controller.rb
@@ -4,11 +4,18 @@ class Api::Settings::GeneralController < ApiController
     before_action :require_someone!
 
     def index
+      if @current_someone.class.name == "Specialist"
+        @current_someone[:contact_phone] = general_params[:contact_phone]
+      end
       respond_with @current_someone, serializer: ::Settings::GeneralSerializer
     end
 
     def update
-      if @current_someone.update(general_params)
+      if @current_someone.class.name == "Specialist"
+        general_params[:phone] = general_params[:contact_phone]
+        general_params.delete!(:contact_phone)
+      end
+      if @current_someone.update(params)
         respond_with @current_someone, serializer: ::Settings::GeneralSerializer, status: :ok
       else
         respond_with errors: @current_someone.errors, status: :unprocessable_entity
@@ -18,6 +25,6 @@ class Api::Settings::GeneralController < ApiController
     private
 
     def general_params
-      params.require(:business).permit(:time_zone, :country, :state, :city, :contact_phone)
+      params.permit(:time_zone, :country, :state, :city, :contact_phone)
     end
   end
