056159f058e4c22704f9e381b1d4144c5cef806f
oscarhandsome <altair5555@gmail.com>
2021-06-04 19:41:56 +0600

    fixed styles for input view

--- app/javascript/packs/vue/business/marketplace/components/MarketPlaceSearchInput.vue
+++ app/javascript/packs/vue/business/marketplace/components/MarketPlaceSearchInput.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   .col-md-12
     .row.py-2
-      .col-sm-12
-        b-form-group(label="Search" label-for="search-input")
-           b-form-input#search-input(v-model="search" placeholder="Enter keywords, skills, etc." @keyup.enter="addTag")
+      .col-md-6.col-sm-12
+        b-form-group(label="Search" label-for="search-input" label-class="label" class="search-form")
+          b-icon(icon="search" class="search-form__icon")
+          b-form-input#search-input(v-model="search" placeholder="Enter keywords, skills, etc." @keyup.enter="addTag" class="search-form__input")
         b-form-group
           b-badge.mr-2(variant="none" v-for="(tag, idx) in values" :key="tag")
             | {{ tag }}
