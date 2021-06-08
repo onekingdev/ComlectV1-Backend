fcb3125a480b40a5ce6a4570f5201c72f2251026
oscarhandsome <altair5555@gmail.com>
2021-06-08 16:33:08 +0600

    hide unworking part from marketplace

---
+++ app/javascript/packs/vue/business/settings/Page.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  div.settings
+    .container-fluid
+      .row
+        .col-md-3 test1
+        .col-md-9 test2
+
+</template>
+
+<script>
+  import Loading from '@/common/Loading/Loading'
+
+  export default {
+    components: {
+      Loading,
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+
+    },
+    computed: {
+      loading() {
+        return this.$store.getters.loading;
+      },
+    },
+    mounted() {
+
+    },
+  };
+</script>
+
+<style>
+  @import "./styles.css";
+</style>
