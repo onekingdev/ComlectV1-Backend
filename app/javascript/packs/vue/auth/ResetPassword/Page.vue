d2beef4c1d351e12c8e722202794792c5dd16f00
oscarhandsome <altair5555@gmail.com>
2021-07-01 15:25:48 +0600

    fixed reset password page

--- app/javascript/packs/vue/auth/ResetPassword/Page.vue
+++ app/javascript/packs/vue/auth/ResetPassword/Page.vue
@@ -5,9 +5,10 @@
       main.row#main-content
         .col-xl-4.col-lg-6.col-md-8.m-x-auto
           .card-body.white-card-body.registration
-            Loading
+            // Loading
+            // #step1.form(v-if='!loading' :class="step1 ? 'd-block' : 'd-none'")
             #step1.form(v-if='!loading' :class="step1 ? 'd-block' : 'd-none'")
-              h1.text-center Reset password via email
+              h1.text-center Reset password
               p.text-center Enter the email address used to log in to your Complect
                 br
                 | account and we'll send you a link to reset your password. If you
@@ -25,9 +26,8 @@
                   b-button.w-100(type='submit' variant='dark') Reset
                   hr
                   b-form-group.text-center.forgot-password.m-t-1
-                    .m-t-1
-                      .forgot-password.m-t-1.m-b-2
-                        a.link(data-remote='true' href='/users/sign_in') Cancel
+                    .forgot-password
+                      a.link(data-remote='true' href='/users/sign_in') Cancel
             #step2.form(v-if='!loading' :class="step2 ? 'd-block' : 'd-none'")
               h1.text-center You successfuly reseted password!
               p.text-center You will be redirect to the sign in page!
@@ -37,16 +37,15 @@
 </template>
 
 <script>
-  import Loading from '@/common/Loading/Loading'
+  // import Loading from '@/common/Loading/Loading'
   import TopNavbar from "../components/TopNavbar";
   import ResetPasswordModal from './Modals/ResetPasswordModal'
 
-  const random = Math.floor(Math.random() * 1000);
+  const TIME_FOR_VIEW_MESSAGE = 3000
 
   export default {
-    props: ['industryIds', 'jurisdictionIds', 'subIndustryIds', 'states'],
     components: {
-      Loading,
+      // Loading,
       TopNavbar,
       ResetPasswordModal,
     },
@@ -85,7 +84,6 @@
 
         this.$store.dispatch('resetEmail', dataToSend)
           .then((response) => {
-
             if (response.errors) {
               const properties = Object.keys(response.errors);
               for (const type of Object.keys(response.errors)) {
@@ -93,16 +91,18 @@
                 this.toast('Error', `Form has errors! Please recheck fields! ${error}`)
                 // Object.keys(response.errors[type]).map(prop => response.errors[prop].map(err => this.toast(`Error`, `${prop}: ${err}`)))
               }
-              return
             }
-
             if (!response.errors) {
-              this.userId = response.userid
-              this.toast('Success', `${response.message}`)
+              // this.userId = response.userid
+              // this.toast('Success', `${response.message}`)
 
               // open step 2
               this.step1 = false
               this.step2 = true
+
+              setTimeout(() => {
+                window.location.href = `${window.location.origin}/users/sign_in`
+              }, TIME_FOR_VIEW_MESSAGE)
             }
 
             // setTimeout(() => {
@@ -126,9 +126,9 @@
       },
     },
     computed: {
-      loading() {
-        return this.$store.getters.loading;
-      },
+      // loading() {
+      //   return this.$store.getters.loading;
+      // },
     },
   }
 </script>
