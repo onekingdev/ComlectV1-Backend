30ec36c7fb2012b3db4edf2d59e38453e9bb113a
DDoS King <birb@birbs-iMac.local>
2021-07-16 15:48:26 +0300

    rubocop and remove comments

--- app/controllers/business/annual_reviews_controller.rb
+++ app/controllers/business/annual_reviews_controller.rb
@@ -5,31 +5,15 @@ class Business::AnnualReviewsController < ApplicationController
   before_action :require_business!
 
   def index
-    # render html: content_tag('business-annuals-page', '').html_safe, layout: 'vue_business'
     render html: content_tag('main-layoyt', '').html_safe, layout: 'vue_main_layout'
   end
 
-  #def show
-  #  render html: content_tag('business-annual-general-page',
-  #                           '',
-  #                           ':annual-id': params[:id])
-  #    .html_safe, layout: 'vue_business'
-  #end
-
- def show
+  def show
     render html: content_tag('main-layoyt',
                              '',
                              ':annual-id': params[:id])
       .html_safe, layout: 'vue_main_layout'
   end
-
-  #def revcat
-  #  render html: content_tag('business-annual-review-page',
-  #                           '',
-  #                           ':annual-id': params[:id],
-  #                           ':revcat-id': params[:revcat])
-  #    .html_safe, layout: 'vue_business'
-  #end
 
   def revcat
     render html: content_tag('main-layoyt',
