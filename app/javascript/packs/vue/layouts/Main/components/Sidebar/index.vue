0ac0f9abe2f23f59b7278cb8ca3f28ea10bb13f1
oscarhandsome <altair5555@gmail.com>
2021-07-16 16:53:53 +0600

    fixed main issues with sidebar

--- app/javascript/packs/vue/layouts/Main/components/Sidebar/index.vue
+++ app/javascript/packs/vue/layouts/Main/components/Sidebar/index.vue
@@ -1,77 +1,64 @@
 <template lang="pug">
-  nav.sidebar-menu(v-if="leftSidebar !== 'settings'" :class="{ mobileMenu: toggleMobileMenu }")
-    //.logo
-    //  a.logo__link(href="/")
-    //    img.logo__img(src="/assets/logo-2-white.png" width="81px")
+  nav.sidebar-menu(v-if="leftSidebar !== 'settings'" :class="{ menuClosed: toggleClosedMenu }")
     div.sidebar-menu__central(v-if="leftSidebar === 'default' || userType === 'specialist'")
-      h3.sidebar-menu__title(role="button" v-b-toggle.overview_collapse="true" @click="toggleMobileMenu = false")
+      h3.sidebar-menu__title(role="button" v-b-toggle.overview_collapse="true" @click="toggleClosedMenu = false")
         ion-icon(name='list-outline')
-        span(v-if="!toggleMobileMenu") Overview
+        span Overview
         ion-icon.ml-auto(name='chevron-down-outline')
       b-collapse#overview_collapse(:visible="true")
         ul.sidebar-menu__list
           li.nav-item.sidebar-menu__item(@click="openLink('default')")
             router-link.sidebar-menu__link(:to='`/${userType}`' active-class="active" exact)
-              //ion-icon(name='globe-outline')
               | Dashboard
           li.nav-item.sidebar-menu__item(@click="openLink('default')")
             router-link.sidebar-menu__link(:to='`/${userType}/reminders`' active-class="active")
-              //ion-icon(name='checkbox-outline')
               | Tasks
           li.nav-item.sidebar-menu__item(@click="openLink('default')")
             router-link.sidebar-menu__link(:to='`/${userType}/projects`' active-class="active")
-              //ion-icon(name='list-outline')
               | Projects
       div(v-if="userType !== 'specialist'")
-        h3.sidebar-menu__title(role="button" v-b-toggle.program_management_collapse="true" @click="toggleMobileMenu = false")
+        h3.sidebar-menu__title(role="button" v-b-toggle.program_management_collapse="true" @click="toggleClosedMenu = false")
           ion-icon(name='document-text-outline')
-          span(v-if="!toggleMobileMenu") Program Management
+          span Program Management
           ion-icon.ml-auto(name='chevron-down-outline')
         b-collapse#program_management_collapse(:visible="true")
           ul.sidebar-menu__list
             li.nav-item.sidebar-menu__item(@click="openLink('default')")
               router-link.sidebar-menu__link(:to='`/${userType}/compliance_policies`' active-class="active")
-                //ion-icon(name='newspaper-outline')
                 | Policies and Procedures
             li.nav-item.sidebar-menu__item(@click="openLink('default')")
               router-link.sidebar-menu__link(:to='`/${userType}/annual_reviews`' active-class="active")
-                //ion-icon(name='document-text-outline')
                 | Annual Review
             li.nav-item.sidebar-menu__item(@click="openLink('default')")
               router-link.sidebar-menu__link(:to='`/${userType}/risks`' active-class="active")
-                //ion-icon(name='warning-outline')
                 | Risk Register
-      //.sidebar-menu__separator
       div(class="dropdown-divider")
-      .nav-item.sidebar-menu__item.sidebar-menu__item_setting(@click="openLink('settings')")
-        router-link.sidebar-menu__link(:to='`/${userType}/settings`' active-class="active")
-          ion-icon(name='settings-outline')
-          span(v-if="!toggleMobileMenu") Settings
+      router-link.sidebar-menu__link(:to='`/${userType}/settings`' active-class="active")
+        h3.sidebar-menu__title.sidebar-menu__title_settings
+          ion-icon(name='settings-outline' @click="openLink('settings')")
+          span Settings
     div.sidebar-menu__central(v-if="userType !== 'specialist' && leftSidebar === 'documents'")
      h3.sidebar-menu__title(role="button" v-b-toggle.files="")
       ion-icon(name='document-text-outline')
-      span(v-if="!toggleMobileMenu") Files
+      span Files
       ion-icon.ml-auto(name='chevron-down-outline')
      b-collapse#files(:visible="true")
        ul.sidebar-menu__list
          li.nav-item.sidebar-menu__item(@click="openLink('documents')")
            router-link.sidebar-menu__link(:to='`/${userType}/file_folders`' active-class="active" exact)
-             //ion-icon(name='folder-outline')
              | Book and records
          li.nav-item.sidebar-menu__item(@click="openLink('documents')")
            router-link.sidebar-menu__link(:to='`/${userType}/exam_management`' active-class="active")
-             //ion-icon(name='search-outline')
              | Exam Management
-         div(class="dropdown-divider")
-         li.nav-item.sidebar-menu__item(@click="openLink('settings')")
-           router-link.sidebar-menu__link(:to='`/${userType}/settings`' active-class="active")
-             ion-icon(name='settings-outline')
-             | Settings
-    //.sidebar-menu__separator
+     div(class="dropdown-divider")
+     router-link.sidebar-menu__link(:to='`/${userType}/settings`' active-class="active")
+        h3.sidebar-menu__title.sidebar-menu__title_settings
+          ion-icon(name='settings-outline' @click="openLink('settings')")
+          span Settings
     div(class="dropdown-divider")
-    button.sidebar-menu__btn(@click="toggleMobileMenu = !toggleMobileMenu")
-      span(v-if="!toggleMobileMenu") Collapse menu
-      ion-icon(:name="!toggleMobileMenu ? 'arrow-back-circle-outline' : 'arrow-forward-circle-outline'")
+    button.sidebar-menu__btn(@click="toggleClosedMenu = !toggleClosedMenu")
+      span Collapse menu
+      ion-icon(:name="!toggleClosedMenu ? 'arrow-back-circle-outline' : 'arrow-forward-circle-outline'")
 </template>
 
 <script>
@@ -79,7 +66,7 @@
     data() {
       return {
         userType: '',
-        toggleMobileMenu: false
+        toggleClosedMenu: false
       }
     },
     created() {
