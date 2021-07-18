fe6861d01f7f5fa2c011b6dd159c24a0efb63318
oscarhandsome <altair5555@gmail.com>
2021-07-18 18:24:16 +0600

    fixed partly central css issues

--- app/javascript/packs/vue/business/tasks/components/TasksItem.vue
+++ app/javascript/packs/vue/business/tasks/components/TasksItem.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   tr
     td
-      b-icon.m-r-1.pointer(font-scale="1" :icon="item.done_at ? 'check-circle-fill' : 'check-circle'" @click="toggleDone(item)" v-bind:class="{ done_task: item.done_at }")
-      //ion-icon.m-r-1.pointer(@click="toggleDone(item)" v-bind:class="{ done_task: item.done_at }" name='checkmark-circle-outline')
-      TaskModalEdit.link(:taskProp="item" @saved="$emit('saved')")
-        span(v-if="!item.done_at" ) {{ item.body }}
-        s(v-else) {{ item.body }}
+      .name
+        b-icon.m-r-1.pointer(font-scale="1" :icon="item.done_at ? 'check-circle-fill' : 'check-circle'" @click="toggleDone(item)" v-bind:class="{ done_task: item.done_at }")
+        //ion-icon.m-r-1.pointer(@click="toggleDone(item)" v-bind:class="{ done_task: item.done_at }" name='checkmark-circle-outline')
+        TaskModalEdit.link(:taskProp="item" @saved="$emit('saved')")
+          span(v-if="!item.done_at" ) {{ item.body }}
+          s(v-else) {{ item.body }}
     td(v-if="!shortTable")
       .d-flex.align-items-center.link
         ion-icon.m-r-1(name="list-circle-outline")
