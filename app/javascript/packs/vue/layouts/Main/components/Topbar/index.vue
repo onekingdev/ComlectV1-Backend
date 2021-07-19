6c635d957bbd637a06493c086f223936f44152fb
oscarhandsome <altair5555@gmail.com>
2021-07-19 19:11:32 +0600

    moved styles inside vue

--- app/javascript/packs/vue/layouts/Main/components/Topbar/index.vue
+++ app/javascript/packs/vue/layouts/Main/components/Topbar/index.vue
@@ -23,7 +23,7 @@
           li.nav-item.topbar-menu__item.d-sm-none(v-if="userType === 'specialist'" @click="openLink('default')")
             router-link.topbar-menu__link(:to='`/${userType}/projects-marketpalce`' active-class="active") Browse Projects
     // Right aligned nav items
-    b-navbar-nav.flex-row.align-items-center.ml-auto.h-100
+    b-navbar-nav.flex-row.align-items-center.ml-auto
       router-link.btn.btn-warning.btn-topbar.btn-topbar_find(v-if="userType === 'business'" :to='`/${userType}/specialists`') Find an Expert
       router-link.btn.btn-warning.btn-topbar.btn-topbar_find(v-if="userType === 'specialist'" :to='`/${userType}/projects-marketpalce`') Browse Projects
       router-link.btn.btn-topbar.btn-topbar_notify(:to='`/${userType}/settings/notification-center`')
