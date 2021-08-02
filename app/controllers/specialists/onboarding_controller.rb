cfd2c7e4a8cb147a1137ae643692b9e6c63e9cc5
oscarhandsome <altair5555@gmail.com>
2021-08-02 12:54:56 +0600

    fixed controller for specialist

--- app/controllers/specialists/onboarding_controller.rb
+++ app/controllers/specialists/onboarding_controller.rb
@@ -6,19 +6,23 @@ class Specialists::OnboardingController < ApplicationController
   before_action :authenticate_user!
   before_action :require_specialist!
 
+  # def index
+  #   render html: content_tag('specialist-onboarding-page', '',
+  #                            ':industry-ids': Industry.all.map(&proc { |ind|
+  #                                                                 { id: ind.id,
+  #                                                                   name: ind.name }
+  #                                                               }).to_json,
+  #                            ':jurisdiction-ids': Jurisdiction.all.map(&proc { |ind|
+  #                                                                         { id: ind.id,
+  #                                                                           name: ind.name }
+  #                                                                       }).to_json,
+  #                            ':sub-industry-ids': sub_industries(true).to_json,
+  #                            ':states': State.fetch_all_usa.to_json,
+  #                            ':timezones': timezones_array.to_json).html_safe, layout: 'vue_onboarding'
+  # end
+
   def index
-    render html: content_tag('specialist-onboarding-page', '',
-                             ':industry-ids': Industry.all.map(&proc { |ind|
-                                                                  { id: ind.id,
-                                                                    name: ind.name }
-                                                                }).to_json,
-                             ':jurisdiction-ids': Jurisdiction.all.map(&proc { |ind|
-                                                                          { id: ind.id,
-                                                                            name: ind.name }
-                                                                        }).to_json,
-                             ':sub-industry-ids': sub_industries(true).to_json,
-                             ':states': State.fetch_all_usa.to_json,
-                             ':timezones': timezones_array.to_json).html_safe, layout: 'vue_onboarding'
+    render html: content_tag('auth-layoyt', '').html_safe, layout: 'vue_onboarding'
   end
 
   def subscribe
