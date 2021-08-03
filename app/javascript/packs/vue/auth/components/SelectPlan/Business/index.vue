2878a2b30a926531ae3ef272de48166291bc9714
oscarhandsome <altair5555@gmail.com>
2021-08-04 01:11:39 +0600

    fixed prices page

--- app/javascript/packs/vue/auth/components/SelectPlan/Business/index.vue
+++ app/javascript/packs/vue/auth/components/SelectPlan/Business/index.vue
@@ -15,20 +15,21 @@
       b-card.billing-plan(v-for='(plan, index) in billingPlans' :class="[index === 0 ? 'billing-plan_low' : '', index === 1 ? 'billing-plan_medium' : '', index === 2 ? 'billing-plan_high' : '' ]" :key=`index`)
         b-button.m-b-20(type='button' :variant="currentPlan.status && currentPlan.id === index+1 ? 'dark' : 'outline-primary'" @click="$emit('openDetails', plan)")
           | {{ currentPlan.status && currentPlan.id === index+1 ? 'Current' : 'Select' }} Plan
-        b-card-text
+        b-card-text.billing-plan__common-info
           h4.billing-plan__name {{ plan.name }}
           p.billing-plan__descr {{ plan.description }}
           h5.billing-plan__coast {{ billingTypeSelected === 'annually' ?  plan.coastMonthlyDiscountFormatted : plan.coastMonthlyFormatted }}
+            span.billing-plan__discount(v-if="plan.id !== 1 && billingTypeSelected === 'annually'")  &nbsp;{{ plan.coastMonthlyFormatted }}
           p.billing-plan__users(v-if="plan.id === 1") {{ plan.users }}
           p.billing-plan__users(v-if="plan.id !== 1 && billingTypeSelected === 'annually'")
-            span.billing-plan__discount {{ plan.coastMonthlyFormatted }}
-            span.text-success &nbsp;{{ plan.coastAnnuallyFormatted }}
+            //span.billing-plan__discount {{ plan.coastMonthlyFormatted }}
+            span.text-success.font-weight-bold &nbsp;{{ plan.coastAnnuallyFormatted }}
           p.billing-plan__users(v-if="plan.id !== 1") {{ billingTypeSelected === 'annually' ?  plan.usersCount + ' free users plus $' + plan.additionalUserAnnually + '/year per person' : plan.usersCount + ' free users plus $' + plan.additionalUserMonthly + '/mo per person' }}
-          hr
-          ul.list-unstyled.billing-plan__list
-            li.billing-plan__item(v-for="feature in plan.features")
-              b-icon.billing-plan__icon(icon="check-circle-fill" variant="success")
-              span(v-html="feature")
+        hr
+        ul.list-unstyled.billing-plan__list
+          li.billing-plan__item(v-for="feature in plan.features")
+            b-icon.billing-plan__icon(icon="check-circle-fill" variant="success")
+            span(v-html="feature")
     .row
       .col
         .text-right.m-t-30
