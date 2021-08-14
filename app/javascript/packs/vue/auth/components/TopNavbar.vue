d419f82d9458a8fb8d31a898565a69c7adf3ba8c
Sasha Stadnik <stadniklksndr@gmail.com>
2021-08-14 05:03:45 +0300

    fix-sign-out (#514)

--- app/javascript/packs/vue/auth/components/TopNavbar.vue
+++ app/javascript/packs/vue/auth/components/TopNavbar.vue
@@ -40,25 +40,38 @@
         }
       },
       methods: {
+        removeCurrentUser() {
+          localStorage.removeItem('app.currentUser');
+          localStorage.removeItem('app.currentUser.token');
+          localStorage.removeItem('app.currentUser.userType');
+          localStorage.removeItem('app.currentUser.paymentMethod');
+          window.location.href = `${window.location.origin}`
+        },
         signOut() {
-          const accessTokenLocalStorage = localStorage.getItem('app.currentUser.token') ? localStorage.getItem('app.currentUser.token') : ''
+          const accessTokenLocalStorage = localStorage.getItem('app.currentUser.token')
+            ? localStorage.getItem('app.currentUser.token')
+            : ''
+
           fetch('/api/users/sign_out', {
             method: 'DELETE',
-            headers: {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': JSON.parse(accessTokenLocalStorage)},
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': JSON.parse(accessTokenLocalStorage)
+            }
           })
-            .then(response => response.json())
-            .then(data => {
-              localStorage.removeItem('app.currentUser');
-              localStorage.removeItem('app.currentUser.token');
-              localStorage.removeItem('app.currentUser.userType');
-              localStorage.removeItem('app.currentUser.paymentMethod');
-              window.location.href = `${window.location.origin}`
-            })
-            .catch(error => console.error(error))
+          .then(response => {
+            if (!response.ok && response.status === 404) {
+              // Remove current user if user not found
+              this.removeCurrentUser();
+            }
 
-          // this.$store.dispatch('auth/signOut')
-          //   .then(response => console.log(response))
-          //   .catch(error => console.error(error))
+            return response.json()
+          })
+          .then(data => {
+            this.removeCurrentUser();
+          })
+          .catch(error => console.error(error))
         }
       },
       computed: {
@@ -69,7 +82,9 @@
           return this.$store.getters.getUser;
         },
         userName() {
-          const fullName = this.currentUser.contact_first_name ? `${this.currentUser.contact_first_name} ${this.currentUser.contact_last_name}` : `${this.currentUser.first_name} ${this.currentUser.last_name}`
+          const fullName = this.currentUser.contact_first_name
+            ? `${this.currentUser.contact_first_name} ${this.currentUser.contact_last_name}`
+            : `${this.currentUser.first_name} ${this.currentUser.last_name}`
           return (fullName !== 'undefined undefined') ? fullName : ''
         },
         mainUrl() {
