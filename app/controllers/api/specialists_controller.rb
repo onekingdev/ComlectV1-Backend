b23d9bd5edea78c809d7e36e4ed4deb8d6275432
Sasha Stadnik <stadniklksndr@gmail.com>
2021-08-22 01:37:00 +0300

    add hot fix (#542)

--- app/controllers/api/specialists_controller.rb
+++ app/controllers/api/specialists_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::SpecialistsController < ApiController
-  before_action :require_specialist!, only: :update
+  before_action :require_specialist!, only: %i[update current]
   skip_before_action :authenticate_user!, only: :create
 
   def create
