da62add7ab007a036ac7921691fcbf64b84e0f12
Hanh <41274252+nguyenht10@users.noreply.github.com>
2021-08-29 00:22:25 -0400

    Update FoldersModalCreate.vue (#572)
    * Update FoldersModalCreate.vue
    
    Change to toast text and request to @oscarhandsome to remove toasts. Please review @oscarhandsome.
    
    Errors that are due to missing validations = in form red error message not toasts
    
    * fixed toasts to alert
    
    * added mapActions
    
    Co-authored-by: oscarhandsome <altair5555@gmail.com>

--- app/javascript/packs/vue/business/filefolders/modals/FoldersModalCreate.vue
+++ app/javascript/packs/vue/business/filefolders/modals/FoldersModalCreate.vue
@@ -4,8 +4,10 @@
       slot
 
     b-modal.fade(:id="modalId" title="New Folder")
+      b-alert.m-b-20(v-if="error" variant="danger" show) {{ error }}
+
       .row
-        .col-12.m-b-2
+        .col-12
           label.form-label Name
           input.form-control(v-model="file_folder.name" type="text" placeholder="Enter the name of your folder" ref="input" v-on:keyup.enter="submit")
           Errors(:errors="errors.name")
@@ -16,7 +18,7 @@
 </template>
 
 <script>
-  import { mapGetters } from "vuex"
+  import { mapActions, mapGetters } from "vuex"
   import Errors from '@/common/Errors'
 
   const rnd = () => Math.random().toFixed(10).toString().replace('.', '')
@@ -37,7 +39,8 @@
         file_folder: {
           name: '',
         },
-        errors: []
+        errors: {},
+        error: ''
       }
     },
     computed: {
@@ -46,33 +49,39 @@
       })
     },
     methods: {
+      ...mapActions({
+        createFolder: 'filefolders/createFolder',
+      }),
       async submit(e) {
         e.preventDefault();
+        this.error = ''
 
         if (!this.file_folder.name) {
-          this.toast('Error', `N - Required field`, true)
+          this.error = 'Required field'
           return
         }
         if (this.file_folder.name.length <= 3) {
-          this.errors.push('Name is very short, must be more 3 characters.');
-          this.toast('Error', 'N - Name must have more than 3 characters.', true)
+          // this.errors.push('Name is very short, must be more 3 characters.');
+          this.error = 'Name must have more than 3 characters.'
           return;
         }
 
         if(this.parentId) this.file_folder.parent_id = this.parentId
 
         try {
-          const response = await this.$store.dispatch('filefolders/createFolder', this.file_folder)
+          const response = await this.createFolder(this.file_folder)
           if (response.errors) {
             this.toast('Error', `Folder has not been created. Please try again. ${response.status}`, true)
             Object.keys(response.errors)
               .map(prop => response.errors[prop].map(err => this.toast(`Error`, `${prop}: ${err}`)))
             return
           }
-          this.toast('Success', `Folder has been created.`)
-          this.$emit('saved')
-          this.$bvModal.hide(this.modalId)
-          this.file_folder.name = ''
+          if (!response.errors) {
+            this.toast('Success', `Folder has been created.`)
+            this.$emit('saved')
+            this.$bvModal.hide(this.modalId)
+            this.file_folder.name = ''
+          }
         } catch (error) {
           this.toast('Error', error.message, true)
         }
