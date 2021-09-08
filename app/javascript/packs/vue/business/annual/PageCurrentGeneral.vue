26a032e70bda2d8a82d1c4cf0bfd6aedc985fb2c
DDoS King <birb@birbs-iMac.local>
2021-09-08 18:00:18 +0300

    Review in/complete toast

--- app/javascript/packs/vue/business/annual/PageCurrentGeneral.vue
+++ app/javascript/packs/vue/business/annual/PageCurrentGeneral.vue
@@ -327,7 +327,7 @@ export default {
             }
           })
           .catch((error) => console.error(error))
-        this.toast('Success', "Internal review marked as complete!")
+        this.toast('Success', `Internal review marked as ${this.review.complete ? 'in' : ''}complete!`)
         await this.getCurrentReviewReview(this.annualId)
       } catch (error) {
         this.toast('Error', error.message, true)
