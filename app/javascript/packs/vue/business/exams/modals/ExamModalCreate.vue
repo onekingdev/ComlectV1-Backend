5930aeaabd4e191ba822f2ddb95c73f016a854de
truong.loc.binh <truonglocbinh93@gmail.com>
2021-09-13 05:49:32 +0700

    Validate when Create new Exam (#664)
    Co-authored-by: truong loc binh <binhtruong@beeketing.net>

--- app/javascript/packs/vue/business/exams/modals/ExamModalCreate.vue
+++ app/javascript/packs/vue/business/exams/modals/ExamModalCreate.vue
@@ -7,14 +7,17 @@
       .row
         .col-12.m-b-2
           label.form-label Name
-          input.form-control(v-model="exam_management.name" type="text" ref="input" @keyup="onChange")
+          input.form-control(v-model.trim="exam_management.name" type="text" ref="input" @keyup="onChange")
+          Errors(:errors="errors.name")
       .row.m-b-2
         .col-6
           label.form-label Start Date
-          DatePicker(v-model="exam_management.starts_on" :options="datepickerOptions")
+          b-form-datepicker(v-model="exam_management.starts_on" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en")
+          Errors(:errors="errors.starts_on")
         .col-6
-          label.form-label Due Date
-          DatePicker(v-model="exam_management.ends_on" :options="datepickerOptions")
+          label.form-label End Date
+          b-form-datepicker(v-model="exam_management.ends_on" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en")
+          Errors(:errors="errors.ends_on")
 
       template(slot="modal-footer")
         button.btn.btn-link(@click="$bvModal.hide(modalId)") Cancel
@@ -58,13 +61,29 @@
           this.submit(e)
         }
       },
-      async submit(e) {
-        e.preventDefault();
-
-        if (!this.exam_management.name || !this.exam_management.starts_on || !this.exam_management.ends_on) {
-          this.toast('Error', `Please check all fields!`, true)
-          return
+      validate() {
+        this.errors = {}
+        const name = this.exam_management.name
+        const startAt = this.exam_management.starts_on
+        const endAt = this.exam_management.ends_on
+        const requiredFields = ['name', 'starts_on', 'ends_on']
+        
+        for(let i = 0; i <= requiredFields.length - 1; i++) {
+          if (!this.exam_management[requiredFields[i]]) this.errors[requiredFields[i]] = ['Required field']
         }
+
+        if (name && name.length <= 3) {
+          this.errors['name'] = ['Name must be more than 3 characters']
+        }
+        
+        if (startAt && endAt && (startAt > endAt)) {
+          this.errors['ends_on'] = ['Date must occur before start date']
+        }
+      },
+      async submit(e) {
+        e.preventDefault()
+        this.validate()
+        if (Object.keys(this.errors).length > 0) return
 
         try {
           await this.$store.dispatch('exams/createExam', this.exam_management)
@@ -77,13 +96,6 @@
             .catch(error => this.toast('Error', `Exam has not been created.`, true) )
         } catch (error) {
           this.toast('Error', error.message, true)
-        }
-      },
-    },
-    computed: {
-      datepickerOptions() {
-        return {
-          min: new Date().toISOString()
         }
       },
     },
