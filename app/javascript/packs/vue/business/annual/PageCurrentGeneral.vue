d86313052394afb02528ec3ba634dd6659a3b7d5
Constantine Gololobov <dev@gololobov.dev>
2021-09-13 00:45:16 +0200

    Date Format (#661)
    Change Date Format to en-US
    + previous fix on same page

--- app/javascript/packs/vue/business/annual/PageCurrentGeneral.vue
+++ app/javascript/packs/vue/business/annual/PageCurrentGeneral.vue
@@ -45,11 +45,11 @@
                   .row
                     .col-6
                       label.form-label Start Date
-                      b-form-datepicker(v-model="review.review_start" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en")
+                      b-form-datepicker(v-model="review.review_start" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en-US")
                       Errors(:errors="errors.review_start")
                     .col-6
                       label.form-label End Date
-                      b-form-datepicker(v-model="review.review_end" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en")
+                      b-form-datepicker(v-model="review.review_end" :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }" locale="en-US")
                       Errors(:errors="errors.review_end")
                 .reviews__card--internal
                   .row
