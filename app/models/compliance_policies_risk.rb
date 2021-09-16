9c75039bf1612aae23f0c15a6963e8b113bb8a87
internet-exploder <72084929+internet-exploder@users.noreply.github.com>
2021-09-16 13:32:55 +0300

    Compliance Policy Risks validations (#679)
    * Compliance Policy Risks validations
    
    * Update compliance_policies_risk.rb
    
    Co-authored-by: DDoS King <birb@birbs-iMac.local>

--- app/models/compliance_policies_risk.rb
+++ app/models/compliance_policies_risk.rb
@@ -3,4 +3,12 @@
 class CompliancePoliciesRisk < ActiveRecord::Base
   belongs_to :compliance_policy
   belongs_to :risk
+  validates :compliance_policy_id, uniqueness: { scope: :risk_id }
+  validate :policy_belongs_to_business
+
+  private
+
+  def policy_belongs_to_business
+    return errors.add :compliance_policy_id if risk.business.id != compliance_policy.business.id
+  end
 end
