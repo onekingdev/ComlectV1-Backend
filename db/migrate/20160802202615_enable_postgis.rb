37a754b9927a6ef4a240c8281766d1feda07de70
DDoS King <birb@birbs-iMac.local>
2021-09-20 21:47:14 +0300

    upgrade postgres for ci and remove postgis

--- db/migrate/20160802202615_enable_postgis.rb
+++ db/migrate/20160802202615_enable_postgis.rb
@@ -3,7 +3,7 @@
 class EnablePostgis < ActiveRecord::Migration[6.0]
   # rubocop:disable Lint/HandleExceptions
   def change
-    enable_extension 'postgis'
+    # enable_extension 'postgis'
   rescue PG::UniqueViolation
   end
   # rubocop:enable Lint/HandleExceptions
--- db/structure.sql
+++ db/structure.sql
@@ -10,20 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
-
-
---
--- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
-
-
---
 -- Name: projects_calculate_budget(); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -61,7 +47,7 @@ CREATE FUNCTION public.set_point_from_lat_lng() RETURNS trigger
 
 SET default_tablespace = '';
 
--- SET default_table_access_method = heap;
+SET default_table_access_method = heap;
 
 --
 -- Name: admin_users; Type: TABLE; Schema: public; Owner: -
