19b3d2d00b81b5c30fa202b39b5d967119d7b549
truong.loc.binh <truonglocbinh93@gmail.com>
2021-09-22 21:38:08 +0700

    174 browse projects (#704)
    Co-authored-by: truong loc binh <binhtruong@beeketing.net>

--- app/serializers/specialist/business_serializer.rb
+++ app/serializers/specialist/business_serializer.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class Specialist::BusinessSerializer < ApplicationSerializer
-  attributes :id, :business_name, :employees, :country, :city, :state, :description, :jurisdictions, :industries, :logo
+  attributes :id, :business_name, :employees, :country, :city, :state, :description, :jurisdictions, :industries, :logo, :aum, :client_account_cnt, :active_member
   has_many :jurisdictions, serializer: JurisdictionSerializer
   has_many :industries, serializer: IndustrySerializer
 
   def logo
     object.logo_url(:thumb)
+  end
+
+  def active_member
+    object.team_members.where(active: true).size
   end
 end
