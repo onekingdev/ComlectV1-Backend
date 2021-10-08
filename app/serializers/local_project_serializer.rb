92bf4d3954a3a776815100a33bcccd591e49440b
truong loc binh <binhtruong@beeketing.net>
2021-10-08 12:11:42 +0700

    Fix assignee not show on task project

--- app/serializers/local_project_serializer.rb
+++ app/serializers/local_project_serializer.rb
@@ -6,6 +6,7 @@ class LocalProjectSerializer < ApplicationSerializer
   has_one :visible_project, serializer: ProjectSerializer
   has_many :specialists, serializer: Business::SpecialistSerializer
   has_many :collaborators, serializer: Business::SpecialistSerializer
+  has_many :reminders, serializer: ReminderSerializer
   attributes :id,
              :business_id,
              :title,
@@ -21,7 +22,6 @@ class LocalProjectSerializer < ApplicationSerializer
              :visible_project,
              :collaborators,
              :hide_on_calendar,
-             :reminders,
              :owner,
              :business_is_collaborator
 
